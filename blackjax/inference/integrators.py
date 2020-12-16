329d46c95e8a8d2821b47bec1ee8aac346b3e2db
Rémi Louf <remilouf@gmail.com>
2020-12-16 10:59:26 +0100

    add velocity verlet integrators

---
+++ blackjax/inference/integrators.py
@@ -0,0 +1,80 @@
+"""Symplectic, time-reversible, integrators for Hamiltonian trajectories."""
+from typing import Callable, NamedTuple
+
+import jax
+import jax.numpy as jnp
+
+__all__ = ["velocity_verlet"]
+
+
+class IntegratorState(NamedTuple):
+    position: jnp.DeviceArray
+    momentum: jnp.DeviceArray
+    log_prob_grad: float
+
+
+def velocity_verlet(potential_fn: Callable, kinetic_energy_fn: Callable) -> Callable:
+    """The velocity Verlet (or Verlet-Störmer) integrator.
+
+    The velocity Verlet is a two-stage palindromic integrator [1]_ of the form
+    (a1, b1, a2, b1, a1) with a1 = 0. It is numerically stable for values of
+    the step size that range between 0 and 2 (when the mass matrix is the
+    identity).
+
+    While the position (a1 = 0.5) and velocity Verlet are the most commonly used
+    in samplers, it is known in the numerical computation literature that the value
+    $a1 \approx 0.1932$ leads to a lower integration error [2,3]_. The authors of [1]_
+    show that the value $a1 \approx 0.21132$ leads to an even higher step acceptance
+    rate, up to 3 times higher than with the standard position verlet (p.22, Fig.4).
+
+    By choosing the velocity verlet we avoid two computations of the gradient
+    of the kinetic energy. We are trading accuracy in exchange, and it is not
+    clear whether this is the right tradeoff.
+
+
+    References
+    ----------
+    .. [1]: Bou-Rabee, Nawaf, and Jesús Marıa Sanz-Serna. "Geometric
+            integrators and the Hamiltonian Monte Carlo method." Acta Numerica 27
+            (2018): 113-206.
+    .. [2]: McLachlan, Robert I. "On the numerical integration of ordinary
+            differential equations by symmetric composition methods." SIAM Journal on
+            Scientific Computing 16.1 (1995): 151-168.
+    .. [3]: Schlick, Tamar. Molecular modeling and simulation: an
+            interdisciplinary guide: Vol. 21. Springer
+            Science & Business Media, 2010.
+
+    """
+    a1 = 0
+    b1 = 0.5
+    a2 = 1 - 2 * a1
+
+    potential_grad_fn = jax.jit(jax.grad(potential_fn))
+    kinetic_energy_grad_fn = jax.jit(jax.grad(kinetic_energy_fn))
+
+    def one_step(state: IntegratorState, step_size: float) -> IntegratorState:
+        position, momentum, log_prob_grad = state
+
+        momentum = jax.tree_util.tree_multimap(
+            lambda momentum, log_prob_grad: momentum - b1 * step_size * log_prob_grad,
+            momentum,
+            log_prob_grad,
+        )
+
+        kinetic_grad = kinetic_energy_grad_fn(momentum)
+        position = jax.tree_util.tree_multimap(
+            lambda position, kinetic_grad: position + a2 * step_size * kinetic_grad,
+            position,
+            kinetic_grad,
+        )
+
+        log_prob_grad = potential_grad_fn(position)
+        momentum = jax.tree_util.tree_multimap(
+            lambda momentum, log_prob_grad: momentum - b1 * step_size * log_prob_grad,
+            momentum,
+            log_prob_grad,
+        )
+
+        return IntegratorState(position, momentum, log_prob_grad)
+
+    return one_step
---
+++ tests/test_integrators.py
@@ -0,0 +1,123 @@
+import jax
+import jax.numpy as jnp
+import numpy
+import pytest
+
+import blackjax.inference.integrators as integrators
+
+
+def HarmonicOscillator(inv_mass_matrix, k=1.0, m=1.0):
+    """Potential and Kinetic energy of an harmonic oscillator."""
+
+    def potential_energy(q):
+        return jnp.sum(0.5 * k * jnp.square(q["x"]))
+
+    def kinetic_energy(p):
+        v = jnp.multiply(inv_mass_matrix, p["x"])
+        return jnp.sum(0.5 * jnp.dot(v, p["x"]))
+
+    return potential_energy, kinetic_energy
+
+
+def FreeFall(inv_mass_matrix, g=1.0):
+    """Potential and kinetic energy of a free-falling object."""
+
+    def potential_energy(q):
+        return jnp.sum(g * q["x"])
+
+    def kinetic_energy(p):
+        v = jnp.multiply(inv_mass_matrix, p["x"])
+        return jnp.sum(0.5 * jnp.dot(v, p["x"]))
+
+    return potential_energy, kinetic_energy
+
+
+def PlanetaryMotion(inv_mass_matrix):
+    """Potential and kinetic energy for planar planetary motion."""
+
+    def potential_energy(q):
+        return -1.0 / jnp.power(q["x"] ** 2 + q["y"] ** 2, 0.5)
+
+    def kinetic_energy(p):
+        z = jnp.stack([p["x"], p["y"]], axis=-1)
+        return 0.5 * jnp.dot(inv_mass_matrix, z ** 2)
+
+    return potential_energy, kinetic_energy
+
+
+algorithms = [{"integrator": integrators.velocity_verlet, "precision": 1e-4}]
+
+
+examples = [
+    {
+        "model": FreeFall,
+        "num_steps": 100,
+        "step_size": 0.01,
+        "q_init": {"x": 0.0},
+        "p_init": {"x": 1.0},
+        "q_final": {"x": 0.5},
+        "p_final": {"x": 1.0},
+        "inv_mass_matrix": jnp.array([1.0]),
+    },
+    {
+        "model": HarmonicOscillator,
+        "num_steps": 100,
+        "step_size": 0.01,
+        "q_init": {"x": 0.0},
+        "p_init": {"x": 1.0},
+        "q_final": {"x": jnp.sin(1.0)},
+        "p_final": {"x": jnp.cos(1.0)},
+        "inv_mass_matrix": jnp.array([1.0]),
+    },
+    {
+        "model": PlanetaryMotion,
+        "num_steps": 628,
+        "step_size": 0.01,
+        "q_init": {"x": 1.0, "y": 0.0},
+        "p_init": {"x": 0.0, "y": 1.0},
+        "q_final": {"x": 1.0, "y": 0.0},
+        "p_final": {"x": 0.0, "y": 1.0},
+        "inv_mass_matrix": jnp.array([1.0, 1.0]),
+    },
+]
+
+
+@pytest.mark.parametrize("do_jit_compile", [True, False])
+@pytest.mark.parametrize("integrator", algorithms)
+@pytest.mark.parametrize("example", examples)
+def test_integrator(example, integrator, do_jit_compile):
+    """Test the numerical accuracy of trajectory integrators.
+
+    We compare the evolution of the trajectory to analytical integration, and
+    the conservation of energy. JAX's default float precision is 32bit; it is
+    possible to change it to 64bit but only at startup. It is thus impossible
+    to test both in the same run; we run the tests with the lower precision.
+
+    """
+    model = example["model"]
+    potential, kinetic_energy = model(example["inv_mass_matrix"])
+    integrator_step = integrator["integrator"]
+
+    step = integrator_step(potential, kinetic_energy)
+    step = jax.jit(step) if do_jit_compile else step
+
+    step_size = example["step_size"]
+
+    q = example["q_init"]
+    p = example["p_init"]
+    initial_state = integrators.IntegratorState(q, p, jax.grad(potential)(q))
+    final_state = jax.lax.fori_loop(
+        0, example["num_steps"], lambda i, state: step(state, step_size), initial_state
+    )
+
+    # We make sure that the particle moved from its initial position.
+    for dimension in final_state.position:
+        init = final_state.position[dimension]
+        final = example["q_final"][dimension]
+        print(example["q_final"], final_state.position)
+        numpy.testing.assert_allclose(init, final, atol=1e-2)
+
+    # We now check the conservation of energy, the property that matters the most in HMC.
+    energy = potential(q) + kinetic_energy(p)
+    new_energy = potential(final_state.position) + kinetic_energy(final_state.momentum)
+    assert energy == pytest.approx(new_energy.item(), integrator["precision"])
