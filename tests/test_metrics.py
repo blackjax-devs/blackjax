55e2b3016a8366229f4d05d018127e28560fb85e
Ravin Kumar <ravinsdrive@gmail.com>
2021-03-19 15:43:54 -0700

    Remove old tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -10,44 +10,6 @@ from blackjax.inference import metrics
 KEY = random.PRNGKey(0)
 DTYPE = "float32"
 
-@pytest.fixture()
-def patch_jax(monkeypatch):
-    """Patches JNP multiply and dot to determine which one is called"""
-    multiply, dot = mock.MagicMock(), mock.MagicMock()
-    monkeypatch.setattr(metrics.jnp, "multiply", multiply)
-    monkeypatch.setattr(metrics.jnp, "dot", dot)
-    return multiply, dot
-
-
-def test_gaussian_euclidean_ndim_1(patch_jax):
-    """Test Gaussian Euclidean Function returns correct function when ndim is one"""
-    multiply, _ = patch_jax
-    x = jnp.ones(shape=1)
-
-    assert multiply.call_count == 0
-    momentum, velocity = metrics.gaussian_euclidean(x)
-
-    try:
-        momentum(KEY, [1])
-    except RecursionError:
-        pass
-    assert multiply.call_count == 1
-
-
-def test_gaussian_euclidean_ndim_2(patch_jax):
-    """Test Gaussian Euclidean Function returns correct function when ndim > 1"""
-    _, dot = patch_jax
-    x = jnp.ones(shape=(2, 2))
-
-    assert dot.call_count == 0
-    momentum, velocity = metrics.gaussian_euclidean(x)
-
-    try:
-        momentum(KEY, [1])
-    except RecursionError:
-        pass
-    assert dot.call_count == 1
-
 
 def test_gaussian_euclidean_ndim_3():
     """Test Gaussian Euclidean Function returns correct function when ndim > 1"""
