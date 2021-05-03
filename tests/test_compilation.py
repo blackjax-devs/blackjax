da448b26a662bf6de5ff7861fb925a07cb35e58e
Rémi Louf <remilouf@gmail.com>
2021-05-03 21:23:15 +0200

    add test on the number of kernel compilations

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -3,23 +3,30 @@
 """Make sure that the potential is only compiled once.
 """
 import jax
+import jax.numpy as jnp
 import jax.scipy as jscipy
 
 import blackjax.hmc as hmc
+import blackjax.nuts as nuts
 
 GLOBAL = {'count': 0}
 
 
 def potential(x):
     GLOBAL['count'] += 1
-    return jscipy.stats.normal(x)
+    return jscipy.stats.norm.logpdf(x)
 
 
 def test_hmc():
     """The reason why this works is because JAX only reads the potential once when compiled?
     """
+    rng_key = jax.random.PRNGKey(0)
+    state = hmc.new_state(1.0, potential)
+    params = hmc.HMCParameters(inv_mass_matrix=jnp.array([1.]))
+
     GLOBAL["count"] = 0
-    kernel = jax.jit(hmc.kernel())
+    kernel = jax.jit(hmc.kernel(potential, params))
+
     for _ in range(10):
         _, rng_key = jax.random.split(rng_key)
         state, _ = kernel(rng_key, state)
@@ -28,8 +35,13 @@ def test_hmc():
 
 
 def test_nuts():
+    rng_key = jax.random.PRNGKey(0)
+    state = hmc.new_state(1.0, potential)
+    params = hmc.HMCParameters(inv_mass_matrix=jnp.array([1.]))
+
     GLOBAL["count"] = 0
-    kernel = jax.jit(nuts.kernel())
+    kernel = jax.jit(nuts.kernel(potential, params))
+
     for _ in range(10):
         _, rng_key = jax.random.split(rng_key)
         state, _ = kernel(rng_key, state)
