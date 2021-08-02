7a7ddd4bad52832abf817433c4fdfea12fc3d922
junpenglao <junpenglao@gmail.com>
2021-08-02 08:52:43 +0200

    Correct comment

--- blackjax/types.py
+++ blackjax/types.py
@@ -7,6 +7,6 @@ Array = Union[np.ndarray, jnp.ndarray]
 PyTree = Union[Array, Iterable[Array], Mapping[Any, Array]]
 # It is not currently tested but we also support recursive PyTrees.
 # Once recursive typing is fully supported (https://github.com/python/mypy/issues/731), we can uncomment the line below.
-# PyTree = Union[Array, Iterable["Array"], Mapping[Any, "Array"]]
+# PyTree = Union[Array, Iterable["PyTree"], Mapping[Any, "PyTree"]]
 
 PRNGKey = Array
