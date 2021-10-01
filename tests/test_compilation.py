19d3d0651469efcfddece21f43467cd73ab4ba1e
junpenglao <junpenglao@gmail.com>
2021-10-01 17:26:25 +0200

    update test_compilation

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -1,6 +1,4 @@
-# Copyright Contributors to the Numpyro project.
-# SPDX-License-Identifier: Apache-2.0
-"""Make sure that the potential is only compiled once.
+"""Make sure that the log probability function is only compiled/trace once.
 """
 import chex
 import jax
@@ -10,47 +8,53 @@ import jax.scipy as jscipy
 import blackjax.hmc as hmc
 import blackjax.nuts as nuts
 
+from absl.testing import absltest
 
-def test_hmc():
-    @chex.assert_max_traces(n=1)
-    def potential(x):
-        return jscipy.stats.norm.logpdf(x)
 
-    rng_key = jax.random.PRNGKey(0)
-    state = hmc.new_state(1.0, potential)
+class CompliationTest(chex.TestCase):
+    def test_hmc(self):
+        @chex.assert_max_traces(n=1)
+        def potential(x):
+            return jscipy.stats.norm.logpdf(x)
 
-    chex.clear_trace_counter()
+        rng_key = jax.random.PRNGKey(0)
+        state = hmc.new_state(1.0, potential)
 
-    kernel = jax.jit(
-        hmc.kernel(
-            potential,
-            step_size=1e-2,
-            inverse_mass_matrix=jnp.array([1.0]),
-            num_integration_steps=10,
+        chex.clear_trace_counter()
+
+        kernel = jax.jit(
+            hmc.kernel(
+                potential,
+                step_size=1e-2,
+                inverse_mass_matrix=jnp.array([1.0]),
+                num_integration_steps=10,
+            )
         )
-    )
 
-    for _ in range(10):
-        rng_key, sample_key = jax.random.split(rng_key)
-        state, _ = kernel(sample_key, state)
+        for _ in range(10):
+            rng_key, sample_key = jax.random.split(rng_key)
+            state, _ = kernel(sample_key, state)
+
+    def test_nuts(self):
+        # Potential function was traced twice as we call potential function
+        # at Step 0 when building a new trajectory in tree doubling.
+        @chex.assert_max_traces(n=2)
+        def potential(x):
+            return jscipy.stats.norm.logpdf(x)
+
+        rng_key = jax.random.PRNGKey(0)
+        state = hmc.new_state(1.0, potential)
+
+        chex.clear_trace_counter()
+
+        kernel = jax.jit(
+            nuts.kernel(potential, step_size=1e-2, inverse_mass_matrix=jnp.array([1.0]))
+        )
+
+        for _ in range(10):
+            rng_key, sample_key = jax.random.split(rng_key)
+            state, _ = kernel(sample_key, state)
 
 
-def test_nuts():
-    # Potential function was traced twice as we call potential function
-    # at Step 0 when building a new trajectory in tree doubling.
-    @chex.assert_max_traces(n=2)
-    def potential(x):
-        return jscipy.stats.norm.logpdf(x)
-
-    rng_key = jax.random.PRNGKey(0)
-    state = hmc.new_state(1.0, potential)
-
-    chex.clear_trace_counter()
-
-    kernel = jax.jit(
-        nuts.kernel(potential, step_size=1e-2, inverse_mass_matrix=jnp.array([1.0]))
-    )
-
-    for _ in range(10):
-        rng_key, sample_key = jax.random.split(rng_key)
-        state, _ = kernel(sample_key, state)
+if __name__ == "__main__":
+    absltest.main()
