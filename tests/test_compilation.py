83a7e38c22f0654cd04012525a834b0d0c831dff
Rémi Louf <remilouf@gmail.com>
2022-01-29 13:07:48 +0100

    Test compilation during warmup

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -53,6 +53,29 @@ class CompilationTest(chex.TestCase):
             rng_key, sample_key = jax.random.split(rng_key)
             state, _ = step(sample_key, state)
 
+    def test_warmup(self):
+        # Log probability function was traced twice as we call it
+        # at Step 0 when building a new trajectory in tree doubling.
+        @chex.assert_max_traces(n=4)
+        def logprob_fn(x):
+            return jscipy.stats.norm.logpdf(x)
+
+        chex.clear_trace_counter()
+
+        rng_key = jax.random.PRNGKey(0)
+
+        warmup = blackjax.window_adaptation(
+            algorithm=blackjax.nuts,
+            logprob_fn=logprob_fn,
+            target_acceptance_rate=0.8,
+        )
+        state, kernel, _ = warmup.run(rng_key, 1.0, 1000)
+        step = jax.jit(kernel)
+
+        for _ in range(10):
+            rng_key, sample_key = jax.random.split(rng_key)
+            state, _ = step(sample_key, state)
+
 
 if __name__ == "__main__":
     absltest.main()
