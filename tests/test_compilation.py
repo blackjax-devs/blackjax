335c8e47b6a6a0bde6454ab3ff144ee8c0b1f216
Rémi Louf <remilouf@gmail.com>
2022-01-31 15:38:38 +0100

    Add regression test on the number of compilations

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -1,4 +1,10 @@
 """Make sure that the log probability function is only compiled/traced once.
+
+These are very important regression tests! JIT-compilation can dominate the
+total sampling time in some complex situations, and we need to make sure that
+internal changes do not trigger more compilations than is necessary. They also
+document the state of the library.
+
 """
 import chex
 import jax
@@ -11,14 +17,21 @@ import blackjax
 
 class CompilationTest(chex.TestCase):
     def test_hmc(self):
-        @chex.assert_max_traces(n=1)
+        """Count the number of times the logprob is compiled when using HMC.
+
+        The logprob is compiled twice: when initializing the state and when
+        compiling the kernel.
+
+        """
+
+        @chex.assert_max_traces(n=2)
         def logprob_fn(x):
             return jscipy.stats.norm.logpdf(x)
 
+        chex.clear_trace_counter()
+
         rng_key = jax.random.PRNGKey(0)
         state = blackjax.hmc.init(1.0, logprob_fn)
-
-        chex.clear_trace_counter()
 
         kernel = blackjax.hmc(
             logprob_fn,
@@ -35,14 +48,14 @@ class CompilationTest(chex.TestCase):
     def test_nuts(self):
         # Log probability function was traced twice as we call it
         # at Step 0 when building a new trajectory in tree doubling.
-        @chex.assert_max_traces(n=1)
+        @chex.assert_max_traces(n=2)
         def logprob_fn(x):
             return jscipy.stats.norm.logpdf(x)
 
+        chex.clear_trace_counter()
+
         rng_key = jax.random.PRNGKey(0)
         state = blackjax.nuts.init(1.0, logprob_fn)
-
-        chex.clear_trace_counter()
 
         kernel = blackjax.nuts(
             logprob_fn, step_size=1e-2, inverse_mass_matrix=jnp.array([1.0])
@@ -54,6 +67,13 @@ class CompilationTest(chex.TestCase):
             state, _ = step(sample_key, state)
 
     def test_hmc_warmup(self):
+        """Counts the number of times the logprob is compiled when using
+        adaptation.
+
+        The adaptation only adds one extra compilation for HMC.
+
+        """
+
         @chex.assert_max_traces(n=3)
         def logprob_fn(x):
             return jscipy.stats.norm.logpdf(x)
@@ -65,17 +85,19 @@ class CompilationTest(chex.TestCase):
         warmup = blackjax.window_adaptation(
             algorithm=blackjax.hmc,
             logprob_fn=logprob_fn,
+            num_steps=1000,
             target_acceptance_rate=0.8,
             num_integration_steps=10,
         )
-        state, kernel, _ = warmup.run(rng_key, 1.0, 1000)
+        state, kernel, _ = warmup.run(rng_key, 1.0)
+        kernel = jax.jit(kernel)
 
         for _ in range(10):
             rng_key, sample_key = jax.random.split(rng_key)
             state, _ = kernel(sample_key, state)
 
     def test_nuts_warmup(self):
-        @chex.assert_max_traces(n=4)
+        @chex.assert_max_traces(n=3)
         def logprob_fn(x):
             return jscipy.stats.norm.logpdf(x)
 
@@ -86,9 +108,10 @@ class CompilationTest(chex.TestCase):
         warmup = blackjax.window_adaptation(
             algorithm=blackjax.nuts,
             logprob_fn=logprob_fn,
+            num_steps=1000,
             target_acceptance_rate=0.8,
         )
-        state, kernel, _ = warmup.run(rng_key, 1.0, 1000)
+        state, kernel, _ = warmup.run(rng_key, 1.0)
         step = jax.jit(kernel)
 
         for _ in range(10):
