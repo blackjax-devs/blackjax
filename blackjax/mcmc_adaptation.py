5a85c9b818cb357f7f9f3206698084b4b166b83f
Rémi Louf <remilouf@gmail.com>
2022-01-31 15:35:53 +0100

    Add foctring for HMC adaptation

--- blackjax/mcmc_adaptation.py
+++ blackjax/mcmc_adaptation.py
@@ -17,6 +17,42 @@ def window_adaptation(
     target_acceptance_rate: float = 0.65,
     **parameters,
 ) -> AdaptationAlgorithm:
+    """Adapt the parameters of algorithms in the HMC family.
+
+    Algorithms in the HMC family on a euclidean manifold depend on the value of
+    at least two parameters: the step size, related to the trajectory
+    integrator, and the mass matrix, linked to the euclidean metric.
+
+    Good tuning is very important, especially for algorithms like NUTS which can
+    be extremely inefficient with the wrong parameter values. This function
+    provides a general-purpose algorithm to tune the values of these parameters.
+    Originally based on Stan's window adaptation, the algorithm has evolved to
+    improve performance and quality.
+
+    Parameters
+    ----------
+    algorithm
+        The algorithm whose parameters are being tuned.
+    logprob_fn
+        The log density probability density function from which we wish to sample.
+    num_steps
+        The number of adaptation steps.
+    is_mass_matrix_diagonal
+        Whether we should adapt a diagonal mass matrix.
+    initial_step_size
+        The initial step size used in the algorithm.
+    target_acceptance_rate
+        The acceptance rate that we target during step size adaptation.
+    **parameters
+        The extra parameters to pass to the algorithm, e.g. the number of
+        integration steps for HMC.
+
+    Returns
+    -------
+    A function that returns the last chain state and a sampling kernel with the
+    tuned parameter values from an initial state.
+
+    """
 
     kernel = algorithm.new_kernel()
 
