b82299ae0f98b42891a2191d42d634c6843fe66d
Michele Gregori <michelegregorits@gmail.com>
2022-05-22 21:29:01 +0200

    refactoring: using jaxopt instead of jax.scipy.optimize

--- blackjax/vi/pathfinder.py
+++ blackjax/vi/pathfinder.py
@@ -4,15 +4,11 @@ import jax
 import jax.random
 import jax.numpy as jnp
 from jax import lax
-from jax._src.scipy.optimize.line_search import line_search
-from jax._src.scipy.optimize._lbfgs import (
-    LBFGSResults,
-    _dot,
-    _two_loop_recursion,
-    _update_history_vectors,
-    _update_history_scalars,
-)
 from jax.flatten_util import ravel_pytree
+import jaxopt
+from jaxopt._src.lbfgs import compute_gamma
+from jaxopt.base import OptStep
+
 from blackjax.types import Array, PRNGKey, PyTree
 
 __all__ = ["PathfinderState", "init", "kernel", "sample_from_state"]
@@ -82,7 +78,12 @@ def init(
         if False output only iteration that maximize ELBO, otherwise output
         all iterations
     lbfgs_kwargs:
-        kwargs passed to the internal call to lbfgs_minimize
+        kwargs passed to the internal call to lbfgs_minimize, avaiable params:
+            maxiter: maximum number of iterations
+            maxcor: maximum number of metric corrections ("history size")
+            ftol: terminates the minimization when `(f_k - f_{k+1}) < ftol`
+            gtol: terminates the minimization when `|g_k|_norm < gtol`
+            maxls: maximum number of line search steps (per iteration)
 
     Returns
     -------
@@ -97,11 +98,15 @@ def init(
     param_dims = initial_position_flatten.shape[0]
 
     if "maxiter" not in lbfgs_kwargs:
-        lbfgs_kwargs["maxiter"] = 10
+        lbfgs_kwargs["maxiter"] = 30
     if "maxcor" not in lbfgs_kwargs:
         lbfgs_kwargs["maxcor"] = 10
+    if "maxls" not in lbfgs_kwargs:
+        # high max line search steps helps optimizing negative log likelihoods
+        # that are sums over (large number of) observations' likelihood
+        lbfgs_kwargs["maxls"] = 1000
 
-    status, history = minimize_lbfgs(
+    (result, status), history = minimize_lbfgs(
         objective_fn, initial_position_flatten, **lbfgs_kwargs
     )
 
@@ -110,8 +115,8 @@ def init(
     # set difference between empty zero states and initial point x0 to zero
     # this is beacuse here we are working with static shapes and keeping all
     # the zero states in the arrays
-    s = jnp.diff(position, axis=0).at[status.k - 1].set(0.0)
-    z = jnp.diff(grad_position, axis=0).at[status.k - 1].set(0.0)
+    s = jnp.diff(position, axis=0).at[-status.iter_num - 1].set(0.0)
+    z = jnp.diff(grad_position, axis=0).at[-status.iter_num - 1].set(0.0)
 
     maxiter = lbfgs_kwargs["maxiter"]
     maxcor = lbfgs_kwargs["maxcor"]
@@ -119,17 +124,18 @@ def init(
     rng_keys = jax.random.split(rng_key, maxiter)
 
     def pathfinder_inner_step(i):
-        i_offset = maxcor + i
-        S = lax.dynamic_slice(s, (i_offset - maxcor, 0), (maxcor, param_dims)).T
-        Z = lax.dynamic_slice(z, (i_offset - maxcor, 0), (maxcor, param_dims)).T
-        alpha = alpha_scalar[i_offset] * jnp.ones(param_dims)
+
+        i_offset = position.shape[0] + i - 2 - maxiter
+        S = lax.dynamic_slice(s, (i_offset - maxcor, 0 + 1), (maxcor, param_dims)).T
+        Z = lax.dynamic_slice(z, (i_offset - maxcor, 0 + 1), (maxcor, param_dims)).T
+        alpha = alpha_scalar[i_offset + 1] * jnp.ones(param_dims)
         beta, gamma = lbfgs_inverse_hessian_factors(S, Z, alpha)
 
         phi, logq = lbfgs_sample(
             rng_key=rng_keys[i],
             num_samples=num_samples,
-            position=position[i_offset],
-            grad_position=grad_position[i_offset],
+            position=position[i_offset + 1],
+            grad_position=grad_position[i_offset + 1],
             alpha=alpha,
             beta=beta,
             gamma=gamma,
@@ -139,10 +145,12 @@ def init(
         elbo = (logp - logq).mean()  # algorithm of figure 9 of the paper
 
         state = PathfinderState(
-            i=jnp.where(i - maxiter + status.k < 0, -1, i - maxiter + status.k),
+            i=jnp.where(
+                i - maxiter + status.iter_num < 0, -1, i - maxiter + status.iter_num
+            ),
             elbo=elbo,
-            position=unravel_fn(position[i_offset]),
-            grad_position=unravel_fn(grad_position[i_offset]),
+            position=unravel_fn(position[i_offset + 1]),
+            grad_position=unravel_fn(grad_position[i_offset + 1]),
             alpha=alpha,
             beta=beta,
             gamma=gamma,
@@ -222,31 +230,25 @@ class LBFGSHistory(NamedTuple):
     gamma: Array
 
 
+_update_history_vectors = (
+    lambda history, new: jnp.roll(history, -1, axis=0).at[-1, :].set(new)
+)
+_update_history_scalar = (
+    lambda history, new: jnp.roll(history, -1, axis=0).at[-1].set(new)
+)
+
+
 def minimize_lbfgs(
-        fun: Callable,
-        x0: Array,
-        maxiter: int = 100,
-        norm: float = jnp.inf,
-        maxcor: float = 10,
-        ftol: float = 2.220446049250313e-09,
-        gtol: float = 1e-05,
-        maxfun: int = None,
-        maxgrad: int = None,
-        maxls: int = 20,
-        ) -> Tuple[LBFGSResults, LBFGSHistory]:
+    fun: Callable,
+    x0: Array,
+    maxiter: int = 30,
+    maxcor: float = 10,
+    gtol: float = 1e-03,
+    ftol: float = 1e-02,
+    maxls: int = 1000,
+) -> Tuple[OptStep, LBFGSHistory]:
     """
-    Licensed under the Apache License, Version 2.0 (the "License");
-    TAKEN FROM https://github.com/google/jax/blob/main/jax/_src/scipy/optimize/_lbfgs.py
-    and added functionality to return optimization path
-
     Minimize a function using L-BFGS
-
-    Implements the L-BFGS algorithm from
-      Algorithm 7.5 from Wright and Nocedal, 'Numerical Optimization', 1999, pg. 176-185
-    And generalizes to complex variables from
-       Sorber, L., Barel, M.V. and Lathauwer, L.D., 2012.
-       "Unconstrained optimization of real functions in complex variables"
-       SIAM Journal on Optimization, 22(3), pp.879-898.
 
     Parameters
     ----------
@@ -257,18 +259,12 @@ def minimize_lbfgs(
         initial guess
     maxiter:
         maximum number of iterations
-    norm:
-        order of norm for convergence check. Default inf.
     maxcor:
         maximum number of metric corrections ("history size")
     ftol:
         terminates the minimization when `(f_k - f_{k+1}) < ftol`
     gtol:
         terminates the minimization when `|g_k|_norm < gtol`
-    maxfun:
-        maximum number of function evaluations
-    maxgrad:
-        maximum number of gradient evaluations
     maxls:
         maximum number of line search steps (per iteration)
 
@@ -278,119 +274,62 @@ def minimize_lbfgs(
     """
     d = len(x0)
     dtype = jnp.dtype(x0)
+    grad_fun = jax.grad(fun)
 
-    # ensure there is at least one termination condition
-    if (maxiter is None) and (maxfun is None) and (maxgrad is None):
-        maxiter = d * 200
+    def cond_fun(inputs):
+        (_, state), i, history = inputs
+        return (
+            (state.error > gtol)
+            & (((history.f[-2] - history.f[-1]) > ftol) | (i == 0))
+            & (i < maxiter)
+        ).all()
 
-    # set others to inf, such that >= is supported
-    if maxfun is None:
-        maxfun = jnp.inf
-    if maxgrad is None:
-        maxgrad = jnp.inf
+    def body_fun(inputs):
+        (params, state), i, history = inputs
 
-    # initial evaluation
-    f_0, g_0 = jax.value_and_grad(fun)(x0)
-    state_initial = LBFGSResults(
-      converged=False,
-      failed=False,
-      k=0,
-      nfev=1,
-      ngev=1,
-      x_k=x0,
-      f_k=f_0,
-      g_k=g_0,
-      s_history=jnp.zeros((maxcor, d), dtype=dtype),
-      y_history=jnp.zeros((maxcor, d), dtype=dtype),
-      rho_history=jnp.zeros((maxcor,), dtype=dtype),
-      gamma=1.,
-      status=0,
-      ls_status=0,
-    )
-
-    history_initial = LBFGSHistory(
-      x=_update_history_vectors(
-          jnp.zeros((maxiter + maxcor + 1, d), dtype=dtype),
-          x0),
-      f=_update_history_scalars(
-          jnp.zeros(maxiter + maxcor + 1, dtype=dtype),
-          f_0),
-      g=_update_history_vectors(
-          jnp.zeros((maxiter + maxcor + 1, d), dtype=dtype),
-          g_0),
-      gamma=_update_history_scalars(
-          jnp.zeros(maxiter + maxcor + 1, dtype=dtype),
-          state_initial.gamma)
-      )
-
-    def cond_fun(args):
-        state, history = args
-        return (~(state.converged)) & (~(state.failed))
-
-    def body_fun(args):
-        state, history = args
-        # find search direction
-        p_k = _two_loop_recursion(state)
-
-        # line search
-        ls_results = line_search(
-          f=fun,
-          xk=state.x_k,
-          pk=p_k,
-          old_fval=state.f_k,
-          gfk=state.g_k,
-          maxiter=maxls,
-        )
-
-        # evaluate at next iterate
-        s_k = ls_results.a_k * p_k
-        x_kp1 = state.x_k + s_k
-        f_kp1 = ls_results.f_k
-        g_kp1 = ls_results.g_k
-        y_k = g_kp1 - state.g_k
-        rho_k_inv = jnp.real(_dot(y_k, s_k))
-        rho_k = jnp.reciprocal(rho_k_inv)
-        gamma = rho_k_inv / jnp.real(_dot(jnp.conj(y_k), y_k))
-
-        # replacements for next iteration
-        status = 0
-        status = jnp.where(state.f_k - f_kp1 < ftol, 4, status)
-        status = jnp.where(state.ngev >= maxgrad, 3, status)  # type: ignore
-        status = jnp.where(state.nfev >= maxfun, 2, status)  # type: ignore
-        status = jnp.where(state.k >= maxiter, 1, status)  # type: ignore
-        status = jnp.where(ls_results.failed, 5, status)
-
-        converged = jnp.linalg.norm(g_kp1, ord=norm) < gtol
-
+        # this is to help optimization when using log-likelihoods, especially for float 32
+        # it resets stepsize of the line search algorithm back to stating value (1.0) if
+        # it get stuck in very small values
         state = state._replace(
-          converged=converged,
-          failed=(status > 0) & (~converged),
-          k=state.k + 1,
-          nfev=state.nfev + ls_results.nfev,
-          ngev=state.ngev + ls_results.ngev,
-          x_k=x_kp1,
-          f_k=f_kp1,
-          g_k=g_kp1,
-          s_history=_update_history_vectors(history=state.s_history, new=s_k),
-          y_history=_update_history_vectors(history=state.y_history, new=y_k),
-          rho_history=_update_history_scalars(history=state.rho_history,
-                                              new=rho_k),
-          gamma=gamma,
-          status=jnp.where(converged, 0, status),
-          ls_status=ls_results.status,
+            stepsize=jnp.where(state.stepsize < 1e-3, 1.0, state.stepsize)
         )
+        last = (state.iter_num % maxcor + maxcor) % maxcor
+        gamma = compute_gamma(state.s_history, state.y_history, last)
+        new_opt = solver.update(params, state)
+
+        i += 1
 
         history = history._replace(
-          x=_update_history_vectors(history=history.x, new=x_kp1),
-          f=_update_history_scalars(history=history.f, new=f_kp1),
-          g=_update_history_vectors(history=history.g, new=g_kp1),
-          gamma=_update_history_scalars(history=history.gamma, new=gamma)
-          )
+            x=_update_history_vectors(history=history.x, new=new_opt.params),
+            f=_update_history_scalar(history=history.f, new=fun(new_opt.params)),
+            g=_update_history_vectors(history=history.g, new=grad_fun(new_opt.params)),
+            gamma=_update_history_scalar(history=history.gamma, new=gamma),
+        )
+        return (new_opt, i, history)
 
-        return state, history
+    history_initial = LBFGSHistory(
+        x=_update_history_vectors(
+            jnp.zeros((maxiter + maxcor + 2, d), dtype=dtype), x0
+        ),
+        f=_update_history_scalar(
+            jnp.zeros((maxiter + maxcor + 2), dtype=dtype), fun(x0)
+        ),
+        g=_update_history_vectors(
+            jnp.zeros((maxiter + maxcor + 2, d), dtype=dtype), grad_fun(x0)
+        ),
+        gamma=_update_history_scalar(jnp.zeros(maxiter + maxcor + 2, dtype=dtype), 1.0),
+    )
 
-    state, history = lax.while_loop(cond_fun, body_fun,
-                                    (state_initial, history_initial))
+    solver = jaxopt.LBFGS(fun=fun, maxiter=maxiter, maxls=maxls, history_size=maxcor)
+    state = solver.init_state(x0)
+    state = state._replace(stepsize=state.stepsize / 1.5)
+    zero_step = OptStep(params=x0, state=state)
+    init_val = (zero_step, 0, history_initial)
+
+    state, _, history = lax.while_loop(
+        cond_fun=cond_fun, body_fun=body_fun, init_val=init_val
+    )
+
     return state, history
 
 
