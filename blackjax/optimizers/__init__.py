cf3c28e29e99ea4a0049a7263fd35df0e34e2115
junpenglao <junpenglao@gmail.com>
2022-06-16 07:29:24 +0200

    Update __init__.py

--- blackjax/optimizers/__init__.py
+++ blackjax/optimizers/__init__.py
@@ -1,17 +1,6 @@
-from .dual_averaging import dual_averaging
-from .lbfgs import (
-    bfgs_sample,
-    lbfgs_inverse_hessian_factors,
-    lbfgs_inverse_hessian_formula_1,
-    lbfgs_inverse_hessian_formula_2,
-    minimize_lbfgs,
-)
+from . import dual_averaging, lbfgs
 
 __all__ = [
     "dual_averaging",
-    "bfgs_sample",
-    "lbfgs_inverse_hessian_factors",
-    "lbfgs_inverse_hessian_formula_1",
-    "lbfgs_inverse_hessian_formula_2",
-    "minimize_lbfgs",
+    "lbfgs,
 ]
