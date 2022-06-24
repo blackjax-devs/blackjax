852dd2181139a4a61a53583cf75a6d37399e789e
adrien <adrien.corenflos@gmail.com>
2022-06-24 18:39:11 +0300

    Add the marginal gradient sampler for latent gaussian models

--- blackjax/kernels.py
+++ blackjax/kernels.py
@@ -527,7 +527,6 @@ class sgld:
         grad_estimator_fn: Callable,
         schedule_fn: Callable,
     ) -> SamplingAlgorithm:
-
         step = cls.kernel(grad_estimator_fn)
 
         def init_fn(position: PyTree, data_batch: PyTree):
@@ -861,7 +860,6 @@ class pathfinder:
         num_samples: int = 200,
         **lbfgs_kwargs,
     ) -> SamplingAlgorithm:
-
         step = cls.kernel()
 
         def init_fn(position: PyTree):
@@ -949,7 +947,6 @@ def pathfinder_adaptation(
         return ((state, adaptation_state), (state, info, adaptation_state.da_state))
 
     def run(rng_key: PRNGKey, position: PyTree):
-
         rng_key_init, rng_key_chain = jax.random.split(rng_key, 2)
 
         init_warmup_state, init_position = init(rng_key, position, initial_step_size)
