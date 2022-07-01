1def7ae833cff9b06e976d1bb02775b3b7fef617
Carlos Iguaran <ciguaran@users.noreply.github.com>
2022-07-01 11:33:52 -0300

    SMC ESS calculation multivariate and multivariable posterior tests. (#244)

--- tests/test_smc_ess.py
+++ tests/test_smc_ess.py
@@ -4,9 +4,11 @@ import itertools
 
 import chex
 import jax
+import jax.numpy as jnp
 import numpy as np
 from absl.testing import absltest, parameterized
-from jax.scipy.stats.norm import logpdf
+from jax.scipy.stats.multivariate_normal import logpdf as multivariate_logpdf
+from jax.scipy.stats.norm import logpdf as univariate_logpdf
 
 import blackjax.smc.ess as ess
 import blackjax.smc.solver as solver
@@ -39,13 +41,59 @@ class SMCEffectiveSampleSizeTest(chex.TestCase):
     @chex.all_variants(with_pmap=False)
     @parameterized.parameters(itertools.product([0.25, 0.5], [100, 1000, 5000]))
     def test_ess_solver(self, target_ess, N):
-        potential_fn = lambda pytree: -logpdf(pytree, scale=0.1)
-        potential = jax.vmap(lambda x: potential_fn(*x), in_axes=[0])
+        potential_fn = lambda pytree: -univariate_logpdf(pytree, scale=0.1)
+        potential = jax.vmap(lambda x: potential_fn(x), in_axes=[0])
+        particles = np.random.normal(0, 1, size=(N, 1))
+        self.ess_solver_test_case(potential, particles, target_ess, N, 1.0)
+
+    @chex.all_variants(with_pmap=False)
+    @parameterized.parameters(itertools.product([0.25, 0.5], [100, 1000, 5000]))
+    def test_ess_solver_multivariate(self, target_ess, N):
+        """
+        Posterior with more than one variable. Let's assume we want to
+        sample from P(x) x ~ N(mean, cov) x in R^{2}
+        """
+        mean = jnp.zeros((1, 2))
+        cov = jnp.diag(jnp.array([1, 1]))
+        potential_fn = lambda pytree: -multivariate_logpdf(pytree, mean=mean, cov=cov)
+        potential = jax.vmap(lambda x: potential_fn(x), in_axes=[0], out_axes=0)
+        particles = np.random.multivariate_normal(
+            mean=[0.0, 0.0], cov=[[1.0, 0.0], [0.0, 1.0]], size=N
+        )
+        self.ess_solver_test_case(potential, particles, target_ess, N, 10.0)
+
+    @chex.all_variants(with_pmap=False)
+    @parameterized.parameters(itertools.product([0.25, 0.5], [100, 1000, 5000]))
+    def test_ess_solver_posterior_signature(self, target_ess, N):
+        """
+        Posterior with more than one variable. Let's assume we want to
+        sample from P(x,y) x ~ N(mean, cov) y ~ N(mean, cov)
+        """
+        mean = jnp.zeros((1, 2))
+        cov = jnp.diag(jnp.array([1, 1]))
+
+        def potential_fn(pytree):
+            return -multivariate_logpdf(
+                pytree[0], mean=mean, cov=cov
+            ) - multivariate_logpdf(pytree[1], mean=mean, cov=cov)
+
+        potential = jax.vmap(potential_fn, in_axes=[0], out_axes=0)
+        particles = [
+            np.random.multivariate_normal(
+                mean=[0.0, 0.0], cov=[[1.0, 0.0], [0.0, 1.0]], size=N
+            ),
+            np.random.multivariate_normal(
+                mean=[0.0, 0.0], cov=[[1.0, 0.0], [0.0, 1.0]], size=N
+            ),
+        ]
+        self.ess_solver_test_case(potential, particles, target_ess, N, 10.0)
+
+    def ess_solver_test_case(self, potential, particles, target_ess, N, max_delta):
         ess_solver_fn = functools.partial(
             ess.ess_solver,
             potential,
             target_ess=target_ess,
-            max_delta=1.0,
+            max_delta=max_delta,
             root_solver=solver.dichotomy,
         )
 
@@ -53,8 +101,6 @@ class SMCEffectiveSampleSizeTest(chex.TestCase):
             functools.partial(ess_solver_fn, use_log_ess=True)
         )
         ess_solver = self.variant(functools.partial(ess_solver_fn, use_log_ess=False))
-
-        particles = np.random.normal(0, 1, size=(N, 1))
         delta_log = log_ess_solver(particles)
         delta = ess_solver(particles)
         assert delta_log > 0
