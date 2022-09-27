b7660aeb60a52e38cc0e2678efcf4cd73f4da4c7
Rémi Louf <remi@thetypicalset.com>
2022-09-27 23:45:40 +0200

    Use versioneer to define version numbers with git tags

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-import codecs
-import os.path
 import sys
 
 import setuptools
+
+import versioneer
 
 # READ README.md for long description on PyPi.
 try:
@@ -13,36 +13,13 @@ except Exception as e:
     long_description = ""
 
 
-# Get the package's version number of the __init__.py file
-def read(rel_path):
-    """Read the file located at the provided relative path."""
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    """Get the package's version number.
-
-    We fetch the version  number from the `__version__` variable located in the
-    package root's `__init__.py` file. This way there is only a single source
-    of truth for the package's version number.
-
-    """
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
 setuptools.setup(
     name="blackjax",
     author="The BlackJAX team",
-    version=get_version("blackjax/__init__.py"),
     description="Flexible and fast inference in Python",
     long_description=long_description,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=setuptools.find_packages(),
     install_requires=[
         "fastprogress>=0.2.0",
