5f05a997b92f5d1b5def0bc5a6a910087cbad32b
Rémi Louf <remi@thetypicalset.com>
2022-10-05 11:46:12 +0200

    Remove chain adaptation

--- blackjax/adaptation/__init__.py
+++ blackjax/adaptation/__init__.py
@@ -1,3 +1,3 @@
-from . import chain_adaptation, meads, pathfinder_adaptation, window_adaptation
+from . import meads, pathfinder_adaptation, window_adaptation
 
-__all__ = ["chain_adaptation", "meads", "window_adaptation", "pathfinder_adaptation"]
+__all__ = ["meads", "window_adaptation", "pathfinder_adaptation"]
