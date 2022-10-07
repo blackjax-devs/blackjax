57c0c75a6d67b650e670efa4e5dc5ad45194fda7
Rémi Louf <remi@thetypicalset.com>
2022-10-07 15:23:11 +0200

    Move gradient computation outside of diffusion integrators
    This simplifies the solvers a lot.

--- blackjax/sgmcmc/diffusion.py
+++ blackjax/sgmcmc/diffusion.py
@@ -1,29 +1,24 @@
 """Solvers for Langevin diffusions."""
-from typing import NamedTuple
-
 import jax
 import jax.numpy as jnp
 
-from blackjax.sgmcmc.gradients import GradientState
 from blackjax.types import PRNGKey, PyTree
 from blackjax.util import generate_gaussian_noise
 
 __all__ = ["overdamped_langevin"]
 
 
-class DiffusionState(NamedTuple):
-    position: PyTree
-    logprob_grad: PyTree
-    grad_estimator_state: GradientState
-
-
-def overdamped_langevin(grad_estimator_fn):
+def overdamped_langevin():
     """Euler solver for overdamped Langevin diffusion."""
 
     def one_step(
-        rng_key: PRNGKey, state: DiffusionState, step_size: float, minibatch: tuple = ()
-    ):
-        position, logprob_grad, grad_estimator_state = state
+        rng_key: PRNGKey,
+        position: PyTree,
+        logprob_grad: PyTree,
+        step_size: float,
+        minibatch: tuple = (),
+    ) -> PyTree:
+
         noise = generate_gaussian_noise(rng_key, position)
         position = jax.tree_util.tree_map(
             lambda p, g, n: p + step_size * g + jnp.sqrt(2 * step_size) * n,
@@ -32,22 +27,12 @@ def overdamped_langevin(grad_estimator_fn):
             noise,
         )
 
-        logprob_grad, gradient_estimator_state = grad_estimator_fn(
-            grad_estimator_state, position, minibatch
-        )
-        return DiffusionState(position, logprob_grad, gradient_estimator_state)
+        return position
 
     return one_step
 
 
-class SGHMCState(NamedTuple):
-    position: PyTree
-    momentum: PyTree
-    logprob_grad: PyTree
-    grad_estimator_state: GradientState
-
-
-def sghmc(grad_estimator_fn, alpha: float = 0.01, beta: float = 0):
+def sghmc(alpha: float = 0.01, beta: float = 0):
     """Solver for the diffusion equation of the SGHMC algorithm [0]_.
 
     References
@@ -59,9 +44,13 @@ def sghmc(grad_estimator_fn, alpha: float = 0.01, beta: float = 0):
     """
 
     def one_step(
-        rng_key: PRNGKey, state: SGHMCState, step_size: float, minibatch: tuple = ()
+        rng_key: PRNGKey,
+        position: PyTree,
+        momentum: PyTree,
+        logprob_grad: PyTree,
+        step_size: float,
+        minibatch: tuple = (),
     ):
-        position, momentum, logprob_grad, grad_estimator_state = state
         noise = generate_gaussian_noise(rng_key, position)
         position = jax.tree_util.tree_map(lambda x, p: x + p, position, momentum)
         momentum = jax.tree_util.tree_map(
@@ -73,9 +62,6 @@ def sghmc(grad_estimator_fn, alpha: float = 0.01, beta: float = 0):
             noise,
         )
 
-        logprob_grad, gradient_estimator_state = grad_estimator_fn(
-            grad_estimator_state, position, minibatch
-        )
-        return SGHMCState(position, momentum, logprob_grad, gradient_estimator_state)
+        return position, momentum
 
     return one_step
