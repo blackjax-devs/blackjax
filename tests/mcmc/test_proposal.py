600792215125a7175a76a614f69016cb63d4f14a
Carlos Iguaran <ciguaran@users.noreply.github.com>
2023-03-31 14:14:22 -0300

    porting tests to chex by using chex runtime assertions

--- tests/mcmc/test_proposal.py
+++ tests/mcmc/test_proposal.py
@@ -2,9 +2,101 @@ import chex
 import jax
 import numpy as np
 import pytest
+from absl.testing import parameterized
 from jax import numpy as jnp
 
+from blackjax.mcmc.proposal import (
+    Proposal,
+    asymmetric_proposal_generator,
+    proposal_from_energy_diff,
+)
 from blackjax.mcmc.random_walk import normal
+
+
+class TestAsymmetricProposalGenerator(chex.TestCase):
+    @chex.all_variants
+    def test_new(self):
+        state = (jnp.array([1, 2, 3]),)
+        new, _ = asymmetric_proposal_generator(None, None, None)
+
+        assert_proposal_equals(
+            self.variant(new)(state), Proposal(state, 0.0, 0.0, -np.inf)
+        )
+
+    @chex.all_variants
+    def test_update(self):
+        def transition_energy(prev, next):
+            return next - prev
+
+        new_proposal = Proposal(jnp.array([1, 2]), 0.0, 2.0, 50)
+
+        @chex.chexify
+        def proposal_factory(prev_energy, new_energy, divergence_threshold, new_state):
+            chex.assert_tree_all_close(prev_energy, -20)
+            chex.assert_tree_all_close(new_energy, 20)
+            chex.assert_tree_all_close(divergence_threshold, 50)
+            chex.assert_tree_all_close(new_state, 50)
+            return new_proposal
+
+        _, update = asymmetric_proposal_generator(
+            transition_energy, 50, proposal_factory
+        )
+
+        proposed = self.variant(update)(30, 50)
+        assert_proposal_equals(proposed, new_proposal)
+
+
+def assert_proposal_equals(left, right):
+    np.testing.assert_allclose(left.energy, right.energy)
+    np.testing.assert_allclose(left.weight, right.weight)
+    np.testing.assert_allclose(left.sum_log_p_accept, right.sum_log_p_accept)
+    np.testing.assert_allclose(left.state, right.state)
+
+
+class TestProposalFromEnergyDiff(chex.TestCase):
+    @chex.all_variants
+    @parameterized.parameters(
+        [
+            (5, 10, 2, True),
+            (5, 10, 4, True),
+            (5, 10, 6, False),
+            (5, 10, 5, False),
+            (10, 5, 2, True),
+            (10, 5, 4, True),
+            (10, 5, 6, False),
+            (10, 5, 5, False),
+        ]
+    )
+    def test_divergence_threshold(self, before, after, threshold, is_divergent):
+        state = None
+        proposal, divergence = self.variant(proposal_from_energy_diff)(
+            5, 10, threshold, state
+        )
+        assert divergence == is_divergent
+
+    @chex.all_variants
+    def test_sum_log_paccept(self):
+        state = None
+        proposal, _ = self.variant(proposal_from_energy_diff)(5, 10, 0, state)
+        np.testing.assert_allclose(proposal.sum_log_p_accept, -5.0)
+
+        proposal, _ = self.variant(proposal_from_energy_diff)(10, 5, 0, state)
+        np.testing.assert_allclose(proposal.sum_log_p_accept, 0.0)
+
+    @chex.all_variants
+    def test_delta_energy_is_nan(self):
+        state = None
+        proposal, _ = self.variant(proposal_from_energy_diff)(np.nan, np.nan, 0, state)
+        assert np.isneginf(proposal.weight)
+
+    @chex.all_variants
+    def test_weight(self):
+        state = jnp.array([1, 2])
+        proposal, _ = self.variant(proposal_from_energy_diff)(5, 10, 0, state)
+
+        np.testing.assert_allclose(state, proposal.state)
+        np.testing.assert_allclose(proposal.weight, -5)
+        np.testing.assert_allclose(proposal.energy, 10)
 
 
 class TestNormalProposalDistribution(chex.TestCase):
--- tests/mcmc/test_random_walk.py
+++ tests/mcmc/test_random_walk.py
@@ -1,5 +1,7 @@
-import unittest
+import functools
+from functools import partial
 
+import chex
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -15,7 +17,8 @@ from blackjax.mcmc.random_walk import (
 )
 
 
-class AdditiveStepTest(unittest.TestCase):
+class AdditiveStepTest(chex.TestCase):
+    @chex.all_variants
     def test_one_step_addition(self):
         """New position is an addition to previous position.
         Since the density == 1, the proposal is accepted.
@@ -24,23 +27,25 @@ class AdditiveStepTest(unittest.TestCase):
         rng_key = jax.random.PRNGKey(0)
         initial_position = jnp.array([50.0])
 
+        @chex.chexify
         def random_step(key, position):
-            assert position == initial_position
+            # chex.assert_tree_all_close(position, initial_position)
             return jnp.array([10.0])
 
         def test_logdensity_accepts(position):
             """
             a logdensity that ges maximized after the step
             """
-            return 0.0 if all(position > 59.0) else 0.5
+            return jnp.where(position > 59.0, 0.0, 0.5).sum()
 
         step = build_additive_step()
-
-        new_state, _ = step(
-            rng_key,
-            random_step,
-            RWState(position=initial_position, logdensity=1.0),
-            test_logdensity_accepts,
+        wrapped = self.variant(
+            partial(
+                step, random_step=random_step, logdensity_fn=test_logdensity_accepts
+            )
+        )
+        new_state, _ = wrapped(
+            rng_key, state=RWState(position=initial_position, logdensity=1.0)
         )
 
         np.testing.assert_allclose(new_state.position, jnp.array([60.0]))
@@ -48,7 +53,8 @@ class AdditiveStepTest(unittest.TestCase):
         assert new_state.position
 
 
-class IRMHTest(unittest.TestCase):
+class IRMHTest(chex.TestCase):
+    @chex.all_variants(with_jit=True)
     def test_proposal_is_independent_of_position(self):
         """New position does not depend on previous"""
         rng_key = jax.random.PRNGKey(0)
@@ -62,21 +68,25 @@ class IRMHTest(unittest.TestCase):
             """
             a logdensity that gets maximized after the step
             """
-            return 0.0 if all(position - 10.0 < 1e-10) else 0.5
+            return jnp.where(position - 10.0 < 1e-10, 0.0, 0.5).sum()
 
         step = build_irmh()
+        wrapped = self.variant(
+            functools.partial(
+                step,
+                proposal_distribution=proposal_distribution,
+                logdensity_fn=test_logdensity_accepts,
+            )
+        )
 
         for previous_position in [initial_position, other_position]:
-            new_state, _ = step(
-                rng_key,
-                proposal_distribution,
-                RWState(position=previous_position, logdensity=1.0),
-                test_logdensity_accepts,
+            new_state, _ = wrapped(
+                rng_key, state=RWState(position=previous_position, logdensity=1.0)
             )
             np.testing.assert_allclose(new_state.position, jnp.array([10.0]))
 
 
-class RMHProposalTest(unittest.TestCase):
+class RMHProposalTest(chex.TestCase):
     def transition_distribution(self, key, position):
         return jnp.array([10.0])
 
@@ -92,6 +102,7 @@ class RMHProposalTest(unittest.TestCase):
     def generate_proposal(self, prev, new):
         return Proposal(new, 0, 0, 0), False
 
+    @chex.all_variants(with_jit=True)
     def test_generate_reject(self):
         """
         Steps from previous state,
@@ -111,7 +122,9 @@ class RMHProposalTest(unittest.TestCase):
             sample_proposal=self.reject,
         )
 
-        sampled_proposal, do_accept, p_accept = generate(rng_key, prev_state)
+        sampled_proposal, do_accept, p_accept = self.variant(generate)(
+            rng_key, prev_state
+        )
 
         assert not do_accept
         assert p_accept == 0.3
@@ -133,19 +146,22 @@ class RMHProposalTest(unittest.TestCase):
         np.testing.assert_allclose(sampled_proposal.state.position, jnp.array([10.0]))
 
 
-class RMHTransitionEnergyTest(unittest.TestCase):
+class RMHTransitionEnergyTest(chex.TestCase):
+    @chex.all_variants(with_jit=True)
     def test_energy(self):
         one_state = RWState(None, 50.0)
         another_state = RWState(None, 30.0)
 
         def proposal_logdensity(new_state, prev_state):
-            return 100 if new_state == one_state else 200
+            return jnp.where(
+                jnp.abs(new_state.logdensity - 50.0) < 1e-10, 100, 200
+            ).sum()
 
-        energy = build_rmh_transition_energy(None)
+        energy = self.variant(build_rmh_transition_energy(None))
         np.testing.assert_allclose(energy(one_state, another_state), -30.0)
         np.testing.assert_allclose(energy(another_state, one_state), -50.0)
 
-        energy = build_rmh_transition_energy(proposal_logdensity)
+        energy = self.variant(build_rmh_transition_energy(proposal_logdensity))
         np.testing.assert_allclose(energy(one_state, another_state), -230)
         np.testing.assert_allclose(energy(another_state, one_state), -150)
 
