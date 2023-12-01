cdbb4f6f6833f33f737fbbb0d273503ae2c73ce6
= <reubenharry@gmail.com>
2023-12-01 20:16:59 +0100

    use effective_sample_size

--- blackjax/adaptation/mclmc_adaptation.py
+++ blackjax/adaptation/mclmc_adaptation.py
@@ -21,10 +21,14 @@ import warnings
 from chex import PRNGKey
 import jax
 import jax.numpy as jnp
-from scipy.fftpack import next_fast_len #type: ignore
+from scipy.fftpack import next_fast_len
+from blackjax.diagnostics import effective_sample_size #type: ignore
 
 from blackjax.mcmc.integrators import IntegratorState, noneuclidean_mclachlan
 from blackjax.mcmc.mclmc import build_kernel, init
+import jax
+import jax.numpy as jnp
+from typing import NamedTuple
 
 
 class MCLMCAdaptationState(NamedTuple):
@@ -33,130 +37,27 @@ class MCLMCAdaptationState(NamedTuple):
     L: float
     step_size: float
 
-
 def ess_corr(x):
-    """Taken from: https://blackjax-devs.github.io/blackjax/diagnostics.html
-    shape(x) = (num_samples, d)"""
-
-    input_array = jnp.array(
-        [
-            x,
-        ]
-    )
-
-    num_chains = 1  # input_array.shape[0]
-    num_samples = input_array.shape[1]
-
-    mean_across_chain = input_array.mean(axis=1, keepdims=True)
-    # Compute autocovariance estimates for every lag for the input array using FFT.
-    centered_array = input_array - mean_across_chain
-    m = next_fast_len(2 * num_samples)
-    ifft_ary = jnp.fft.rfft(centered_array, n=m, axis=1)
-    ifft_ary *= jnp.conjugate(ifft_ary)
-    autocov_value = jnp.fft.irfft(ifft_ary, n=m, axis=1)
-    autocov_value = (
-        jnp.take(autocov_value, jnp.arange(num_samples), axis=1) / num_samples
-    )
-    mean_autocov_var = autocov_value.mean(0, keepdims=True)
-    mean_var0 = (
-        jnp.take(mean_autocov_var, jnp.array([0]), axis=1)
-        * num_samples
-        / (num_samples - 1.0)
-    )
-    weighted_var = mean_var0 * (num_samples - 1.0) / num_samples
-
-    weighted_var = jax.lax.cond(
-        num_chains > 1,
-        lambda _: weighted_var + mean_across_chain.var(axis=0, ddof=1, keepdims=True),
-        lambda _: weighted_var,
-        operand=None,
-    )
-
-    # Geyer's initial positive sequence
-    num_samples_even = num_samples - num_samples % 2
-    mean_autocov_var_tp1 = jnp.take(
-        mean_autocov_var, jnp.arange(1, num_samples_even), axis=1
-    )
-    rho_hat = jnp.concatenate(
-        [
-            jnp.ones_like(mean_var0),
-            1.0 - (mean_var0 - mean_autocov_var_tp1) / weighted_var,
-        ],
-        axis=1,
-    )
-
-    rho_hat = jnp.moveaxis(rho_hat, 1, 0)
-    rho_hat_even = rho_hat[0::2]
-    rho_hat_odd = rho_hat[1::2]
-
-    mask0 = (rho_hat_even + rho_hat_odd) > 0.0
-    carry_cond = jnp.ones_like(mask0[0])
-    max_t = jnp.zeros_like(mask0[0], dtype=int)
-
-    def positive_sequence_body_fn(state, mask_t):
-        t, carry_cond, max_t = state
-        next_mask = carry_cond & mask_t
-        next_max_t = jnp.where(next_mask, jnp.ones_like(max_t) * t, max_t)
-        return (t + 1, next_mask, next_max_t), next_mask
-
-    (*_, max_t_next), mask = jax.lax.scan(
-        positive_sequence_body_fn, (0, carry_cond, max_t), mask0
-    )
-    indices = jnp.indices(max_t_next.shape)
-    indices = tuple([max_t_next + 1] + [indices[i] for i in range(max_t_next.ndim)])
-    rho_hat_odd = jnp.where(mask, rho_hat_odd, jnp.zeros_like(rho_hat_odd))
-    # improve estimation
-    mask_even = mask.at[indices].set(rho_hat_even[indices] > 0)
-    rho_hat_even = jnp.where(mask_even, rho_hat_even, jnp.zeros_like(rho_hat_even))
-
-    # Geyer's initial monotone sequence
-    def monotone_sequence_body_fn(rho_hat_sum_tm1, rho_hat_sum_t):
-        update_mask = rho_hat_sum_t > rho_hat_sum_tm1
-        next_rho_hat_sum_t = jnp.where(update_mask, rho_hat_sum_tm1, rho_hat_sum_t)
-        return next_rho_hat_sum_t, (update_mask, next_rho_hat_sum_t)
-
-    rho_hat_sum = rho_hat_even + rho_hat_odd
-    _, (update_mask, update_value) = jax.lax.scan(
-        monotone_sequence_body_fn, rho_hat_sum[0], rho_hat_sum
-    )
-
-    rho_hat_even_final = jnp.where(update_mask, update_value / 2.0, rho_hat_even)
-    rho_hat_odd_final = jnp.where(update_mask, update_value / 2.0, rho_hat_odd)
-
-    # compute effective sample size
-    ess_raw = num_chains * num_samples
-    tau_hat = (
-        -1.0
-        + 2.0 * jnp.sum(rho_hat_even_final + rho_hat_odd_final, axis=0)
-        - rho_hat_even_final[indices]
-    )
-
-    tau_hat = jnp.maximum(tau_hat, 1 / jnp.log10(ess_raw))
-    ess = ess_raw / tau_hat
-
-    neff = ess.squeeze() / num_samples
-    return 1.0 / jnp.average(1 / neff)
-
-import jax
-import jax.numpy as jnp
-from typing import NamedTuple
+    num_samples = x.shape[0]
+    ess = 0.5 * effective_sample_size(jnp.array([x, x]))
+    ess_per_sample = ess / num_samples
+    return 1.0 / jnp.average(1 / ess_per_sample)
 
 
 
+def mclmc_find_L_and_step_size(kernel, num_steps, initial_state, frac_tune1 = 0.1,
+    frac_tune2 = 0.1,
+    frac_tune3 = 0.1):
 
-def mclmc_find_L_and_step_size(kernel, num_steps, initial_state):
-
-    d = initial_state.position.shape[0]
+    dim = initial_state.position.shape[0]
     dyn = initial_state
-    hyp = MCLMCAdaptationState(jnp.sqrt(d), 
-                                        jnp.sqrt(d) * 0.25, 
+    hyp = MCLMCAdaptationState(jnp.sqrt(dim), 
+                                        jnp.sqrt(dim) * 0.25, 
                                         )
 
-    frac_tune1 = 0.1
-    frac_tune2 = 0.1
-    frac_tune3 = 0.1
+    
     varEwanted = 5e-4
-    tune12p = tune12(kernel, d, False, jnp.array([frac_tune1, frac_tune2]), varEwanted, 1.5, 150)
+    tune12p = tune12(kernel, dim, False, jnp.array([frac_tune1, frac_tune2]), varEwanted, 1.5, 150)
 
     tune3p = tune3(kernel, frac_tune3, 0.4)
 
@@ -168,11 +69,6 @@ def mclmc_find_L_and_step_size(kernel, num_steps, initial_state):
 
     dyn, hyp = run(dyn, hyp, schedule, num_steps)
     return dyn, hyp
-
-# all tuning functions are wrappers, recieving some parameters and returning a function
-# func(dyn, hyp, num_total_steps) -> (dyn, hyp) 
-
-
 
 def run(dyn, hyp, schedule, num_steps):
     
