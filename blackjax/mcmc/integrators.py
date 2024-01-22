479e18f6e6a0007abf7ae9c4b8b4297c51220c9e
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2024-01-22 09:12:46 -0800

    changed BABO to OBABO and changed the default L_proposal

--- blackjax/mcmc/integrators.py
+++ blackjax/mcmc/integrators.py
@@ -400,10 +400,16 @@ def partially_refresh_momentum(momentum, rng_key, step_size, L):
     return unravel_fn((m + z) / jnp.linalg.norm(m + z))
 
 def with_isokinetic_maruyama(integrator):
+    
     def stochastic_integrator(state, step_size, L, rng_key):
-        state, info = integrator(state, step_size)
-        return state._replace(momentum=partially_refresh_momentum(
-        momentum=state.momentum, rng_key=rng_key, L=L, step_size=step_size)), info
+        # partial refreshment
+        _state = state._replace(momentum=partially_refresh_momentum(momentum=state.momentum, rng_key=rng_key, L=L, step_size=step_size * 0.5))
+        # one step of the deterministic dynamics
+        _state, info = integrator(_state, step_size)
+        # partial refreshment
+        _state = state._replace(momentum=partially_refresh_momentum(momentum=_state.momentum, rng_key=rng_key, L=L, step_size=step_size * 0.5))
+        return _state, info
+    
     return stochastic_integrator
 
 
