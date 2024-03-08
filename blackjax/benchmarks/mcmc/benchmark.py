3c338c20cc02c30f1f592758e17239b1259821b6
= <reubenharry@gmail.com>
2024-03-08 14:12:09 +0100

    MOVE BENCHMARKS INSIDE BLACKJAX SRC AND RUN BENCHMARKS IN BENCHMARKS.PY

--- blackjax/benchmarks/mcmc/benchmark.py
+++ blackjax/benchmarks/mcmc/benchmark.py
@@ -1,19 +1,23 @@
 from collections import defaultdict
+import itertools
 from inference_gym import using_jax as gym
 import jax
 import jax.numpy as jnp
 import numpy as np
+from blackjax.mcmc.mhmclmc import rescale
 from sampling_algorithms import samplers
 from inference_models import models
 import blackjax
 from blackjax.util import run_inference_algorithm
-
+# from jax import config
+# config.update("jax_debug_nans", True)
 
 import matplotlib.pyplot as plt
 
 
 def get_num_latents(target):
-  return int(sum(map(np.prod, list(jax.tree_flatten(target.event_shape)[0]))))
+  return target.ndims
+#   return int(sum(map(np.prod, list(jax.tree_flatten(target.event_shape)[0]))))
 
 def err(f_true, var_f, contract = jnp.max):
     """Computes the error b^2 = (f - f_true)^2 / var_f
@@ -29,6 +33,7 @@ def err(f_true, var_f, contract = jnp.max):
     
     def _err(f):
         bsq = jnp.square(f - f_true) / var_f
+        # jax.debug.print("bsq {x}", x=(f - f_true, f_true, f))
         # print(bsq.shape, "shape ASDFADSF \n\n")
         return contract(bsq)
     
@@ -45,14 +50,14 @@ def grads_to_low_error(err_t, low_error= 0.01, grad_evals_per_step= 1):
     
     
     
-def ess(err_t, neff= 10, grad_evals_per_step = 2):
+def ess(err_t, grad_evals_per_step, neff= 100):
     
     low_error = 1./neff
     cutoff_reached = err_t[-1] < low_error
     crossing = find_crossing(err_t, low_error)
     # print(len(err_t), "len err t")
 
-    print("crossing", crossing)
+    # print("crossing", crossing, (crossing * grad_evals_per_step), neff / (crossing * grad_evals_per_step))
     # print((err_t)[-100:], "le")
     
     return (neff / (crossing * grad_evals_per_step)) * cutoff_reached
@@ -62,9 +67,11 @@ def ess(err_t, neff= 10, grad_evals_per_step = 2):
 def find_crossing(array, cutoff):
     """the smallest M such that array[m] < cutoff for all m > M"""
 
-    indices = jnp.argwhere(array > cutoff)
+    b = array > cutoff
+    indices = jnp.argwhere(b)
     if indices.shape[0] == 0:
-        raise Exception("No crossing found")
+        print("\n\n\nNO CROSSING FOUND!!!\n\n\n", array, cutoff)
+        return 1
     # print(jnp.argwhere(array))
     return jnp.max(indices)+1
 
@@ -73,126 +80,122 @@ def cumulative_avg(samples):
 
 
 
-def benchmark(model, sampler):
+# def benchmark(model, sampler):
 
-    # print(find_crossing(jnp.array([0.4, 0.2, 0.3, 0.4, 0.5, 0.2, 0.2]), 0.3))
-    # print(cumulative_avg(jnp.array([[1., 2.], [1.,2.]]).T))
-    # raise Exception
+#     # print(find_crossing(jnp.array([0.4, 0.2, 0.3, 0.4, 0.5, 0.2, 0.2]), 0.3))
+#     # print(cumulative_avg(jnp.array([[1., 2.], [1.,2.]]).T))
+#     # raise Exception
 
-    n = 10000
+#     n = 10000
 
-    identity_fn = model.sample_transformations['identity']
-    # print('True mean', identity_fn.ground_truth_mean)
-    # print('True std', identity_fn.ground_truth_standard_deviation)
-    # print("Empirical mean", samples.mean(axis=0))
-    # print("Empirical std", samples.std(axis=0))
+#     identity_fn = model.sample_transformations['identity']
+#     # print('True mean', identity_fn.ground_truth_mean)
+#     # print('True std', identity_fn.ground_truth_standard_deviation)
+#     # print("Empirical mean", samples.mean(axis=0))
+#     # print("Empirical std", samples.std(axis=0))
 
-    logdensity_fn = model.unnormalized_log_prob
-    d = get_num_latents(model)
-    initial_position = jax.random.normal(jax.random.PRNGKey(0), (d,))
-    samples = sampler(logdensity_fn, n, initial_position, jax.random.PRNGKey(0))
-    # print(samples[-1], samples[0], "samps", samples.shape)
+#     logdensity_fn = model.unnormalized_log_prob
+#     d = get_num_latents(model)
+#     initial_position = jax.random.normal(jax.random.PRNGKey(0), (d,))
+#     samples, num_steps_per_traj = sampler(logdensity_fn, n, initial_position, jax.random.PRNGKey(0))
+#     # print(samples[-1], samples[0], "samps", samples.shape)
 
-    favg, fvar = identity_fn.ground_truth_mean, identity_fn.ground_truth_standard_deviation**2
-    err_t = err(favg, fvar, jnp.average)(cumulative_avg(samples))
-    # print(err_t[-1], "benchmark err_t[0]")
-    ess_per_sample = ess(err_t, grad_evals_per_step=2)
+#     favg, fvar = identity_fn.ground_truth_mean, identity_fn.ground_truth_standard_deviation**2
+#     err_t = err(favg, fvar, jnp.average)(cumulative_avg(samples))
+#     # print(err_t[-1], "benchmark err_t[0]")
+#     ess_per_sample = ess(err_t, grad_evals_per_step=2)
     
-    return ess_per_sample
+#     return ess_per_sample
 
-def benchmark_chains(model, sampler):
+def benchmark_chains(model, sampler, favg, fvar, n=10000, batch=None):
 
-    n = 1000
 
-    identity_fn = model.sample_transformations['identity']
-    logdensity_fn = model.unnormalized_log_prob
+    # print(model)
+    # print(model.sample_transformations.keys())
+    # raise Exception
+    # identity_fn = model.sample_transformations['identity']
+    logdensity_fn = lambda x : -model.nlogp(x)
     d = get_num_latents(model)
-    batch = np.ceil(1000 / d).astype(int)
-    keys = jax.random.split(jax.random.PRNGKey(1), batch)
+    if batch is None:
+        batch = np.ceil(1000 / d).astype(int)
+    key, init_key = jax.random.split(jax.random.PRNGKey(44), 2)
+    keys = jax.random.split(key, batch)
     # keys = jnp.array([jax.random.PRNGKey(0)])
+    init_keys = jax.random.split(init_key, batch)
+    # print(init_keys.shape,)
+    # raise Exception
+    init_pos = jax.vmap(model.sample)(init_keys) # jax.random.normal(key=init_key, shape=(batch, d))
+    # print(init_pos.shape, "init pos")
 
-    samples = jax.vmap(lambda pos, key: sampler(logdensity_fn, n, pos, key))(jnp.ones((batch, d)), keys)
+    samples, params, avg_num_steps_per_traj = jax.vmap(lambda pos, key: sampler(logdensity_fn, n, pos, key))(init_pos, keys)
+    avg_num_steps_per_traj = jnp.mean(avg_num_steps_per_traj, axis=0)
+    # print(samples, samples.shape)
+    # print("\n\n\n\nAVG NUM STEPS PER TRAJ", avg_num_steps_per_traj)
     # print(samples[0][-1], samples[0][0], "samps chain", samples.shape)
-    favg, fvar = identity_fn.ground_truth_mean, identity_fn.ground_truth_standard_deviation**2
+              
+        # identity_fn.ground_truth_mean, identity_fn.ground_truth_standard_deviation**2
     full = lambda arr : err(favg, fvar, jnp.average)(cumulative_avg(arr))
-    err_t = jnp.mean(jax.vmap(full)(samples), axis=0)
+    err_t = jnp.mean(jax.vmap(full)(samples**2), axis=0)
     # err_t = jax.vmap(full)(samples)[1]
     # print(err_t[-1], "benchmark chains err_t[0]")
-    ess_per_sample = ess(err_t, grad_evals_per_step=2)
+    # print(avg_num_steps_per_traj, "AVG\n\n")
+    # raise Exception
+    ess_per_sample = ess(err_t, grad_evals_per_step=2 * avg_num_steps_per_traj)
+
+    # print('True mean', identity_fn.ground_truth_mean)
+    # print('True std', identity_fn.ground_truth_standard_deviation)
+    # print("Empirical mean", samples.mean(axis=[0,1]))
+    # print("Empirical std", samples.std(axis=[0,1]))
+
+    # print(params.L.mean(), params.step_size.mean(), "params")
     
-    return ess_per_sample
+    # print('True E[x^2]', identity_fn.ground_truth_mean)
+    # print('True std[x^2]', identity_fn.ground_truth_standard_deviation)
+
+
+
+    return ess_per_sample, err_t[-1], params
+
+def run_benchmarks(n):
+
+    for model, sampler in itertools.product(models, samplers):
+
+        print(f"\nModel: {model}, Sampler: {sampler}\n")
+
+
+        result, bias, _ = benchmark_chains(models[model], samplers[sampler], n=n, batch=100//models[model].ndims,favg=models[model].E_x2, fvar=models[model].Var_x2)
+        print(f"ESS: {result.item()}")
 
 
 if __name__ == "__main__":
 
-    out = benchmark(models['banana'], samplers['nuts'])
-    print(out)
+    run_benchmarks(5000)
 
-    # Define the models and samplers
-    # models = {'icg' : gym.targets.IllConditionedGaussian(), 'banana' : gym.targets.Banana()}
+    # # Extract the models and samplers from the results dictionary
+    # models = [model for model, _ in results.keys()]
+    # samplers = [sampler for _, sampler in results.keys()]
 
-    # Create an empty list to store the results
-    # results = defaultdict(float)
+    # # Extract the corresponding results
+    # results_values = list(results.values())
 
-    # # Run the benchmark for each model and sampler
-    # for model in models:
-    #     for sampler in samplers:
-    #         # result = benchmark(models[model], samplers[sampler])
-    #         result = benchmark_chains(models[model], samplers[sampler])
-    #         # print(result, result2, "results")
-    #         results[(model, sampler)] = result
+    # # Create a figure with two subplots
+    # fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 
-    # print(results)
+    # # Plot the results in the first subplot
+    # axs[0].bar(range(len(results)), results_values)
+    # axs[0].set_xticks(range(len(results)))
+    # axs[0].set_xticklabels(['{} - {}'.format(model, sampler) for model, sampler in zip(models, samplers)], rotation=90)
+    # axs[0].set_title('Benchmark Results')
 
-    # raise Exception
+    # # Plot the results in the second subplot
+    # axs[1].bar(range(len(results)), results_values, color='orange')
+    # axs[1].set_xticks(range(len(results)))
+    # axs[1].set_xticklabels(['{} - {}'.format(model, sampler) for model, sampler in zip(models, samplers)], rotation=90)
+    # axs[1].set_title('Benchmark Results')
 
-# # Extract the models and samplers from the results dictionary
-# models = [model for model, _ in results.keys()]
-# samplers = [sampler for _, sampler in results.keys()]
+    # # Adjust the layout of the subplots
+    # plt.tight_layout()
 
-# # Extract the corresponding results
-# results_values = list(results.values())
-
-# # Create a figure with two subplots
-# fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-
-# # Plot the results in the first subplot
-# axs[0].bar(range(len(results)), results_values)
-# axs[0].set_xticks(range(len(results)))
-# axs[0].set_xticklabels(['{} - {}'.format(model, sampler) for model, sampler in zip(models, samplers)], rotation=90)
-# axs[0].set_title('Benchmark Results')
-
-# # Plot the results in the second subplot
-# axs[1].bar(range(len(results)), results_values, color='orange')
-# axs[1].set_xticks(range(len(results)))
-# axs[1].set_xticklabels(['{} - {}'.format(model, sampler) for model, sampler in zip(models, samplers)], rotation=90)
-# axs[1].set_title('Benchmark Results')
-
-# # Adjust the layout of the subplots
-# plt.tight_layout()
-
-# # Show the plot
-# plt.show()
-
-
-
-# # Create a list of colors for the samplers
-# colors = ['blue', 'red', 'green', 'orange']
-
-# results2 = [1,1]
-
-# # Create a barplot for results
-# plt.bar(range(len(results)), results, label='Results')
-
-# # Create a barplot for results2 with a different color
-# plt.bar(range(len(results2)), results2, label='Results2', color='orange')
-
-# # Add labels to the x-axis
-# plt.xticks(range(len(results)), ['Model 1 - Sampler 1', 'Model 2 - Sampler 1'])
-
-# # Add a title to the plot
-# plt.title('Benchmark Results')
-
-# # Add a legend to indicate the difference between results and results2
-# plt.legend()
+    # # Show the plot
+    # plt.show()
 
--- blackjax/benchmarks/mcmc/sampling_algorithms.py
+++ blackjax/benchmarks/mcmc/sampling_algorithms.py
@@ -3,7 +3,6 @@
 import jax
 import jax.numpy as jnp
 import blackjax
-from blackjax import adaptation
 from blackjax.mcmc.mhmclmc import rescale
 from blackjax.util import run_inference_algorithm
 import blackjax
@@ -24,8 +23,6 @@ def run_nuts(
 
     (state, params), _ = warmup.run(warmup_key, initial_position, num_steps // 10)
 
-
-    print(params)
     nuts = blackjax.nuts(logdensity_fn=logdensity_fn, step_size=params['step_size'], inverse_mass_matrix= params['inverse_mass_matrix'])
 
     final_state, state_history, info_history = run_inference_algorithm(
@@ -36,7 +33,9 @@ def run_nuts(
         transform=lambda x: x.position,
     )
 
-    return state_history
+    # print("INFO\n\n",info_history.num_integration_steps)
+
+    return state_history, params, info_history.num_integration_steps.mean() 
 
 def run_mclmc(logdensity_fn, num_steps, initial_position, key):
     init_key, tune_key, run_key = jax.random.split(key, 3)
@@ -60,6 +59,8 @@ def run_mclmc(logdensity_fn, num_steps, initial_position, key):
         rng_key=tune_key,
     )
 
+    # jax.debug.print("params {x}", x=blackjax_mclmc_sampler_params)
+
     sampling_alg = blackjax.mclmc(
         logdensity_fn,
         L=blackjax_mclmc_sampler_params.L,
@@ -74,10 +75,12 @@ def run_mclmc(logdensity_fn, num_steps, initial_position, key):
         transform=lambda x: x.position,
     )
 
-    return samples
+    avg_steps_per_traj = 1
+    return samples, blackjax_mclmc_sampler_params, avg_steps_per_traj
 
 
 def run_mhmclmc(logdensity_fn, num_steps, initial_position, key):
+
 
     init_key, tune_key, run_key = jax.random.split(key, 3)
 
@@ -86,9 +89,9 @@ def run_mhmclmc(logdensity_fn, num_steps, initial_position, key):
         position=initial_position, logdensity_fn=logdensity_fn, random_generator_arg=init_key
     )
 
-    kernel = lambda rng_key, state, num_integration_steps, step_size: blackjax.mcmc.mhmclmc.build_kernel(
+    kernel = lambda rng_key, state, avg_num_integration_steps, step_size: blackjax.mcmc.mhmclmc.build_kernel(
                 integrator=blackjax.mcmc.integrators.isokinetic_mclachlan,
-                integration_steps_fn = lambda key: num_integration_steps, 
+                integration_steps_fn = lambda k : jnp.ceil(jax.random.uniform(k) * rescale(avg_num_integration_steps))
             )(
                 rng_key=rng_key, 
                 state=state, 
@@ -103,8 +106,9 @@ def run_mhmclmc(logdensity_fn, num_steps, initial_position, key):
         num_steps=num_steps,
         state=initial_state,
         rng_key=tune_key,
-        frac_tune2=0,
+        # frac_tune2=0,
         frac_tune3=0,
+        # params=MCLMCAdaptationState(L=16.765137, step_size=1.005)
     )
 
     # raise Exception
@@ -134,10 +138,16 @@ def run_mhmclmc(logdensity_fn, num_steps, initial_position, key):
         transform=lambda x: x.position, 
         progress_bar=True)
     
+    
     jax.debug.print("ACCEPTANCE {x}", x = (info.acceptance_rate.shape, jnp.mean(info.acceptance_rate,)))
     
-    return out
+    # jax.debug.print("THING\n\n {x}",x=jnp.mean(info.num_integration_steps))
+    # raise Exception
+    return out, blackjax_mclmc_sampler_params, L/step_size
 
 # we should do at least: mclmc, nuts, unadjusted hmc, mhmclmc, langevin
 
-samplers = {'mclmc' : run_mclmc, 'mhmclmc': run_mhmclmc, 'nuts' : run_nuts}
+samplers = {
+    'mclmc' : run_mclmc, 
+    # 'mhmclmc': run_mhmclmc, 
+    'nuts' : run_nuts}
