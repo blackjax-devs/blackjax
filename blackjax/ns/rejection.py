ba677323568958bf3cb55ebc020be3e34366c6ca
yallup <david.yallup@gmail.com>
2024-08-11 23:41:48 +0100

    preliminary loop

--- blackjax/ns/rejection.py
+++ blackjax/ns/rejection.py
@@ -1,4 +1,5 @@
 # Copyright 2024- Will Handley & David Yallup
+from functools import partial
 from typing import Callable
 
 import jax
@@ -6,15 +7,16 @@ import jax.numpy as jnp
 
 import blackjax.ns.base as base
 from blackjax.base import SamplingAlgorithm
+from blackjax.ns.base import build_kernel, init
 from blackjax.types import ArrayLikeTree, PRNGKey
-
-from blackjax.ns.base import init, build_kernel
 
 __all__ = ["init", "as_top_level_api", "build_kernel"]
 
 
 def create_fn(rng_key, particles, logL_fn, logL_star, create_parameters):
-    num_particles, ndims = jax.tree_util.tree_flatten(particles)[0][0].shape
+    # num_particles, ndims = jax.tree_util.tree_flatten(particles)[0][0].shape
+    # ndims = jax.tree_util.tree_flatten(particles)[0][0].shape
+    num_particles, ndims = particles.shape
 
     def body_fun(carry, xs):
         def cond_fun(carry):
@@ -27,7 +29,7 @@ def create_fn(rng_key, particles, logL_fn, logL_star, create_parameters):
             particle = jax.random.uniform(subkey, (ndims,))
             logL = logL_fn(particle)
             return rng_key, logL, particle
-        
+
         rng_key = carry
         rng_key, step_rng = jax.random.split(rng_key)
         _, final_logL, particle = jax.lax.while_loop(
@@ -42,23 +44,46 @@ def create_fn(rng_key, particles, logL_fn, logL_star, create_parameters):
     return new_particles[0], new_particles[1]
 
 
-def delete_fn(rng_key, logL, delete_parameters):
-    idx = logL > logL.min()
-    return idx
+def delete_fn(rng_key, logL, delete_parameters, n_delete):
+    val, idx = jax.lax.top_k(-logL, n_delete)
+    return val, idx
 
 
 def as_top_level_api(
     logL_fn: Callable,
+    n_delete: int = 1,
 ) -> SamplingAlgorithm:
-    """Implements a rejection sampling nested sampling algo
+    """Implements the (basic) user interface for the Adaptive Tempered SMC kernel.
 
     Parameters
     ----------
     logL_fn: Callable
         A function that assigns a weight to the particles.
+    create_fn: Callable
+        Function that takes an array of keys and particles and returns
+        new particles.
+    delete_fn: Callable
+        Function that takes an array of keys and particles and deletes some
+        particles.
     Returns
     -------
     A ``SamplingAlgorithm``.
 
     """
-    return base.as_top_level_api(logL_fn, create_fn, delete_fn)
+
+    delete_func = partial(delete_fn, n_delete=n_delete)
+
+    kernel = build_kernel(
+        logL_fn,
+        create_fn,
+        delete_func,
+    )
+
+    def init_fn(position: ArrayLikeTree, rng_key=None):
+        del rng_key
+        return init(position, logL_fn, {})
+
+    def step_fn(rng_key: PRNGKey, state):
+        return kernel(rng_key, state)
+
+    return SamplingAlgorithm(init_fn, step_fn)
--- test_ns.py
+++ test_ns.py
@@ -1,15 +1,20 @@
-import matplotlib.pyplot as plt
-import jax
 from datetime import date
+
+import jax
+import matplotlib.pyplot as plt
+
+import blackjax.progress_bar
 
 rng_key = jax.random.PRNGKey(0)
 
 
-import numpy as np
 import jax.numpy as jnp
+import numpy as np
 from jax.scipy.stats import multivariate_normal
 
 import blackjax
+
+d = 2
 
 
 def loglikelihood(x):
@@ -20,10 +25,17 @@ algo = blackjax.rejection_ns(loglikelihood)
 
 n_samples = 100
 rng_key, init_key, sample_key = jax.random.split(rng_key, 3)
-state = jax.random.uniform(init_key, (n_samples, 1))
+state = jax.random.uniform(init_key, (n_samples, d))
 state = algo.init(state, loglikelihood)
 
+# from jax_tqdm import scan_tqdm
+from blackjax.progress_bar import progress_bar_scan
 
+n_steps = 1000
+
+
+# @scan_tqdm(100)
+@progress_bar_scan(n_steps)
 def one_step(carry, xs):
     state, k = carry
     k, subk = jax.random.split(k, 2)
@@ -31,15 +43,21 @@ def one_step(carry, xs):
     return (state, k), dead_point
 
 
-with jax.disable_jit():
-    n_iter = 100
-    iterations = jnp.arange(n_iter)
-    res = jax.lax.scan(one_step, (state, rng_key), length=n_iter)
+iterations = jnp.arange(n_steps)
+(live, _), dead = jax.lax.scan((one_step), (state, rng_key), iterations)
+
+# with jax.disable_jit():
+#     n_iter = 10
+#     iterations = jnp.arange(n_iter)
+#     res = jax.lax.scan((one_step), (state, rng_key), iterations)
 
 
-final = res[1].update_info["particles"]
-live = res[0][0][0]
+dead_points = dead.particles.squeeze()
+live_points = live.particles.squeeze()
 
-plt.hist(final.flatten(), bins=30, density=True)
-plt.hist(live.flatten(), bins=30, density=True)
+plt.scatter(*dead_points.T, label="Dead points", color="C1")
+plt.scatter(*live_points.T, label="Live points", color="C0")
+
+# plt.loglog()
+plt.legend()
 plt.show()
