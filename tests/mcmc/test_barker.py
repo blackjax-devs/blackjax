7b31071c25f558ab581218329ec21724c2ad02bb
Ismael Mendoza <imendoza@umich.edu>
2024-09-01 15:49:34 -0500

    Add test to ensure correctness of precond matrix
    We add a new test to barker.py to ensure that our implementation of
    the preconditioning matrix is correct. We follow Appendix G in the
    paper that mentions that algorithm 4 and 5 (which we implemented)
    should be equivalent to rescaling the parameters and the logdensity
    in a specific way. We implement both approaches when using the barker
    proposal to infer the mean and sigma of a normal distribution. We
    check that with two different random seeds the chains outputted are
    equivalent up to some tolerance.
    
    We also patch the original test in this file by adding an identity
    mass matrix.

--- tests/mcmc/test_barker.py
+++ tests/mcmc/test_barker.py
@@ -1,9 +1,15 @@
+import functools
+
 import chex
 import jax
 import jax.numpy as jnp
+import jax.scipy.stats as stats
+import numpy as np
 from absl.testing import absltest, parameterized
 
+import blackjax
 from blackjax.mcmc.barker import _barker_pdf, _barker_sample_nd
+from blackjax.util import run_inference_algorithm
 
 
 class BarkerSamplingTest(chex.TestCase):
@@ -17,9 +23,13 @@ class BarkerSamplingTest(chex.TestCase):
             jnp.array([1.0, -2.0, 10.0, 0.0]),
             0.5,
         )
+        # will have no effect on original test with no preconditioning matrix
+        inv_mass_matrix = jnp.eye(4)
 
         keys = jax.random.split(key, n_samples)
-        samples = jax.vmap(lambda k: _barker_sample_nd(k, m, a, scale))(keys)
+        samples = jax.vmap(
+            lambda k: _barker_sample_nd(k, m, a, scale, inv_mass_matrix)
+        )(keys)
         # Check that the emprical mean and the mean computed as sum(x * p(x) dx) are close
         _test_samples_vs_pdf(samples, lambda x: _barker_pdf(x, m, a, scale))
 
@@ -49,6 +59,70 @@ def _test_samples_vs_pdf(samples, pdf):
     chex.assert_trees_all_close(
         samples_squrared_mean, pdf_squared_mean, atol=1e-2, rtol=1e-2
     )
+
+
+class BarkerPreconditioiningTest(chex.TestCase):
+    @parameterized.parameters([1234, 5678])
+    def test_preconditioning_matrix(self, seed):
+        """Test two different ways of using pre-conditioning matrix has exactly same effect.
+
+        We follow the discussion in Appendix G of the Barker 2020 paper.
+        """
+        from blackjax.mcmc.barker import _get_mass_matrix_sqrt
+
+        key = jax.random.key(seed)
+        init_key, inference_key = jax.random.split(key, 2)
+
+        # setup some 2D multivariate normal model
+        # setup sampling mean and cov
+        true_x = jnp.array([0.0, 1.0])
+        data = jax.random.normal(init_key, shape=(1000,)) * true_x[1] + true_x[0]
+        assert data.shape == (1000,)
+
+        # some non-diagonal positive-defininte matrix for pre-conditioning
+        inv_mass_matrix = jnp.array([[1, 0.1], [0.1, 1]])
+        C_t, C_t_inv = _get_mass_matrix_sqrt(inv_mass_matrix)
+
+        # define barker kernel two ways
+        # non-scaled, use pre-conditioning
+        def logdensity(x, data):
+            mu_prior = stats.norm.logpdf(x[0], loc=0, scale=1)
+            sigma_prior = stats.uniform.logpdf(x[1], 0.0, 3.0)
+            return mu_prior + sigma_prior + jnp.sum(stats.norm.logcdf(data, x[0], x[1]))
+
+        logposterior_fn1 = functools.partial(logdensity, data=data)
+        barker1 = blackjax.barker_proposal(logposterior_fn1, 1e-1, inv_mass_matrix)
+        state1 = barker1.init(true_x)
+
+        # scaled, trivial pre-conditioning
+        def scaled_logdensity(x_scaled, data, C_t):
+            return logdensity(C_t.dot(x_scaled), data)
+
+        logposterior_fn2 = functools.partial(scaled_logdensity, data=data, C_t=C_t)
+        barker2 = blackjax.barker_proposal(logposterior_fn2, 1e-1, jnp.eye(2))
+
+        true_x_trans = C_t_inv.dot(true_x)
+        state2 = barker2.init(true_x_trans)
+
+        _, states1 = run_inference_algorithm(
+            rng_key=inference_key,
+            initial_state=state1,
+            inference_algorithm=barker1,
+            transform=lambda state, info: state.position,
+            num_steps=1000,
+        )
+
+        _, states2 = run_inference_algorithm(
+            rng_key=inference_key,
+            initial_state=state2,
+            inference_algorithm=barker2,
+            transform=lambda state, info: state.position,
+            num_steps=1000,
+        )
+
+        # states should be the exact same with same random key after transforming
+        states2_trans = C_t.dot(states2.T).T
+        np.testing.assert_allclose(states1, states2_trans, atol=1e-2)
 
 
 if __name__ == "__main__":
