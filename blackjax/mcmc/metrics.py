3755e6b063874dc4a70f4200b44dcb278159965b
Adrien Corenflos <adrien.corenflos@gmail.com>
2024-09-16 20:19:31 +0100

    Merged comments from Junpeng

--- blackjax/mcmc/metrics.py
+++ blackjax/mcmc/metrics.py
@@ -187,7 +187,7 @@ def gaussian_euclidean(
         return turning_at_left | turning_at_right
 
     def scale(
-        position: ArrayLikeTree, element: ArrayLikeTree, inv: bool
+        position: ArrayLikeTree, element: ArrayLikeTree, inv: ArrayLikeTree
     ) -> ArrayLikeTree:
         """Scale elements by the mass matrix.
 
@@ -279,7 +279,7 @@ def gaussian_riemannian(
         # return turning_at_left | turning_at_right
 
     def scale(
-        position: ArrayLikeTree, element: ArrayLikeTree, inv: bool
+        position: ArrayLikeTree, element: ArrayLikeTree, inv: ArrayLikeTree
     ) -> ArrayLikeTree:
         """Scale elements by the mass matrix.
 
