Fixed prior sign issue and other shape problems

blackjax/ns/ss_ns.py
@@ -97,7 +97,6 @@ def build_kernel(
         )
 
         def mcmc_step(i, carry):
-            print(i)
             rng_key, new_pos, new_logl = carry
             rng_key, vertical_slice_key = jax.random.split(rng_key)
             logpi = logprior_fn(new_pos)
@@ -116,18 +115,11 @@ def build_kernel(
 
         # Initialize the loop
         rng_key, vertical_slice_key = jax.random.split(rng_key)
-        logpi = logprior_fn(state.sampler_state.particles[idx])
+        new_pos = state.sampler_state.particles[idx]
+        logpi = logprior_fn(new_pos)
         logpi0 = logpi + jnp.log(jax.random.uniform(vertical_slice_key, shape=(idx.shape[0],)))
-        rng_key, horizontal_slice_key = jax.random.split(rng_key)
-        new_pos, new_logl = horizontal_slice_proposal(
-            horizontal_slice_key,
-            state.sampler_state.particles[idx],
-            state.parameter_override["cov"],
-            loglikelihood_fn,
-            logL0,
-            logprior_fn,
-            logpi0,
-        )
+        new_logl = state.sampler_state.logL[idx]
+
         # Run the jax.lax.fori_loop
         rng_key, new_pos, new_logl = jax.lax.fori_loop(0, num_mcmc_steps, mcmc_step, (rng_key, new_pos, new_logl))
 
@@ -176,7 +168,7 @@ def horizontal_slice_proposal(key, x0, cov, logL, logL0, logpi, logpi0):
     def expand_l(carry):
         l, within = carry
         l = l + within[:, None] * n
-        within = jnp.logical_and(logL(l) > logL0, logpi0 > logpi(l))
+        within = jnp.logical_and(logL(l) > logL0, logpi0 < logpi(l))
         return l, within
 
     def cond_fun_l(carry):
@@ -191,7 +183,7 @@ def horizontal_slice_proposal(key, x0, cov, logL, logL0, logpi, logpi0):
     def expand_r(carry):
         r, within = carry
         r = r - within[:, None] * n
-        within = jnp.logical_and(logL(r) > logL0, logpi0 > logpi(r))
+        within = jnp.logical_and(logL(r) > logL0, logpi0 < logpi(r))
         return r, within
 
     def cond_fun_r(carry):
@@ -209,11 +201,11 @@ def horizontal_slice_proposal(key, x0, cov, logL, logL0, logpi, logpi0):
         u = jax.random.uniform(subkey, shape=(x0.shape[0],))
         x1 = l + u[:, None] * (r - l)
         logLx1 = logL(x1)
-        within_new = jnp.logical_and(logLx1 > logL0, logpi0 > logpi(x1))[..., None]
-        s = (jnp.sum((x1 - x0) * (r - l), axis=-1) > 0)[:, None]
-        condition_l = (~within_new[:, 0]) & (~s[:, 0])
+        within_new = jnp.logical_and(logLx1 > logL0, logpi0 < logpi(x1))
+        s = (jnp.sum((x1 - x0) * (r - l), axis=-1) > 0)
+        condition_l = (~within_new) & (~s)
         l = jnp.where(condition_l[:, None], x1, l)
-        condition_r = (~within_new[:, 0]) & s[:, 0]
+        condition_r = (~within_new) & s
         r = jnp.where(condition_r[:, None], x1, r)
         return l, r, x1, logLx1, key, within_new
 

test_ns.py
@@ -1,3 +1,5 @@
+%load_ext autoreload
+%autoreload 2
 import multiprocessing
 import os
 from datetime import date
@@ -46,7 +48,7 @@ n_samples = 500
 n_delete = num_cores
 rng_key, init_key, sample_key = jax.random.split(rng_key, 3)
 
-prior = distrax.MultivariateNormalDiag(loc=jnp.zeros(d), scale_diag=jnp.ones(d))
+prior = distrax.MultivariateNormalDiag(loc=jnp.zeros(d), scale_diag=2*jnp.ones(d))
 
 
 ##################################################################################
@@ -87,7 +89,7 @@ algo = blackjax.ss_ns(
     parameter_update_fn=mcmc_parameter_update_fn,
     n_delete=20,
     initial_parameters=init_params,
-    num_mcmc_steps=5,
+    num_mcmc_steps=5*d,
 
 )
 
@@ -98,7 +100,7 @@ state = algo.init(initial_state, loglikelihood)
 # request 1000 steps of the NS kernel, currently this is fixed, and compresses for n_delete * n_steps rounds
 # simplest design pattern is to put this in an outer while loop, and break when some convergence criteria is met
 # currently there is no safety check in this compression so it can hang with too many steps, or not a good enough inner kernel
-n_steps = 1000
+n_steps = 300
 
 
 @progress_bar_scan(n_steps)
@@ -113,16 +115,21 @@ def one_step(carry, xs):
 # run the ns kernel
 ##################################################################################
 
-# iterations = jnp.arange(n_steps)
-# (live, _), dead = jax.lax.scan((one_step), (state, rng_key), iterations)
+iterations = jnp.arange(n_steps)
+(live, _), dead = jax.lax.scan((one_step), (state, rng_key), iterations)
 
-
-# comment out the above scan and uncomment this for debugging
-with jax.disable_jit():
-    for i in range(10):
-        rng_key, sample_key = jax.random.split(rng_key)
-        state, info = algo.step(sample_key, state)
-
+##with jax.disable_jit():
+#plt.plot(state[0].particles[:, 0], state[0].particles[:, 1], "o")
+#for _ in range(10):
+#    state, info = algo.step(sample_key, state)
+#plt.plot(state[0].particles[:, 0], state[0].particles[:, 1], "o")
+#
+#
+## comment out the above scan and uncomment this for debugging
+#for i in range(10):
+#    rng_key, sample_key = jax.random.split(rng_key)
+#    state, info = algo.step(sample_key, state)
+#
 ##################################################################################
 # Collect the samples into anesthetic objects
 ##################################################################################
@@ -140,10 +147,22 @@ samples = ns.NestedSamples(
     ),
 )
 samples.to_csv("samples.csv")
+from anesthetic import read_csv
+samples = read_csv("samples.csv")
+samples.gui()
+
+
+
 lzs = samples.logZ(100)
 # print(samples.logZ())
 print(f"logZ = {lzs.mean():.2f} ± {lzs.std():.2f}")
 from lsbi.model import ReducedLinearModel
+samples.gui()
+# convert dataframe to normal floats
+samples.logL = samples.logL.astype(float)
+samples.logL_birth = samples.logL_birth.astype(float)
+samples.gui()
+samples.dtypes
 
 model = ReducedLinearModel(
     mu_L=like_mean,