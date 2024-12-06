438afbd7b155210bcb8bf69a683a5ba0b27e73e8
yallup <david.yallup@gmail.com>
2024-12-06 17:53:33 +0000

    include ns in top level api import

--- blackjax/__init__.py
+++ blackjax/__init__.py
@@ -37,6 +37,7 @@ from .smc import adaptive_tempered
 from .smc import inner_kernel_tuning as _inner_kernel_tuning
 from .smc import partial_posteriors_path as _partial_posteriors_smc
 from .smc import tempered
+from .ns import adaptive
 from .vi import meanfield_vi as _meanfield_vi
 from .vi import pathfinder as _pathfinder
 from .vi import schrodinger_follmer as _schrodinger_follmer
@@ -121,6 +122,8 @@ adaptive_tempered_smc = generate_top_level_api_from(adaptive_tempered)
 tempered_smc = generate_top_level_api_from(tempered)
 inner_kernel_tuning = generate_top_level_api_from(_inner_kernel_tuning)
 partial_posteriors_smc = generate_top_level_api_from(_partial_posteriors_smc)
+
+adaptive_ns = generate_top_level_api_from(adaptive)
 
 smc_family = [tempered_smc, adaptive_tempered_smc, partial_posteriors_smc]
 "Step_fn returning state has a .particles attribute"
