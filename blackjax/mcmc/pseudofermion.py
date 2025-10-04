bffe235a244db88d296c1b60209b7a844dc31e17
Reuben Harry Cohn-Gordon <reubenh@login33.chn.perlmutter.nersc.gov>
2025-10-04 08:50:57 -0700

    fix

--- blackjax/mcmc/pseudofermion.py
+++ blackjax/mcmc/pseudofermion.py
@@ -38,43 +38,18 @@ def init(position, logdensity_fn, pseudofermion, init_1, init_2, rng_key  ):
     return {'x': state_b, 'y': state_pf}
 
 
-# def as_top_level_api(
-#     kernel_1,
-#     kernel_2,
-#     init_1,
-#     init_2,
-#     logdensity_fn: Callable,
-# ) -> SamplingAlgorithm:
+def as_top_level_api(
+    kernel_1,
+    kernel_2,
+    init_1,
+    init_2,
+    logdensity_fn: Callable,
+) -> SamplingAlgorithm:
     
-#     # kernel = build_kernel(integrator, divergence_threshold)
+    # kernel = build_kernel(integrator, divergence_threshold)
 
-#     def init_fn(position: ArrayLikeTree, pseudofermion, rng_key=None):
-#         del rng_key
-#         return init(position, logdensity_fn, pseudofermion, init_1, init_2, rng_key)
+    return None
 
-#     def step_fn(rng_key: PRNGKey, state):
-#         next_state, info = kernel_1(
-#             rng_key,
-#             state,
-#             logdensity_fn(state.pseudofermion),
-#             # step_size,
-#             # inverse_mass_matrix,
-#             # max_num_doublings,
-#             # num_integration_steps=num_integration_steps,
-#         )
-#         new_pseudofermion = kernel_2(next_state.position, state.pseudofermion)
-#         # new_fermion_matrix = get_fermion_matrix_fn(next_state.position)
-#         full_state = GibbsState(
-#             position=next_state.position,
-#             momentum=None,
-#             # momentum=next_state.momentum,
-#             logdensity=next_state.logdensity,
-#             logdensity_grad=next_state.logdensity_grad,
-#             pseudofermion=new_pseudofermion,
-#         )
-#         # jax.debug.print("count {x}", x=full_state.count)
-#         return full_state, info
-
-#     return SamplingAlgorithm(init_fn, step_fn)
+    return SamplingAlgorithm(init_fn, step_fn)
 
 
