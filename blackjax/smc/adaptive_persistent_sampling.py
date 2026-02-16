Fix ESS computation in build_kernel to use log scale

blackjax/smc/adaptive_persistent_sampling.py
@@ -109,7 +109,7 @@ def build_kernel(
             )
 
             ess_val = persistent_sampling.compute_persistent_ess(log_weights)
-            return ess_val - target_val
+            return jnp.log(ess_val) - target_val
 
         # in case no solution is found, delta is set to 0 using nan_to_num,
         # this allows for new particles to be added to the persistent ensemble